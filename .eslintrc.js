eea1e745050325af25e356b10ed79cd416c95cee
Vladyslav Polets <46418908+vpolets@users.noreply.github.com>
2020-08-10 18:16:44 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = {
     jest: true
   },
   rules: {
-    'no-proto': 0
+    'no-proto': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
   },
   parser: '@typescript-eslint/parser',
   plugins: [
