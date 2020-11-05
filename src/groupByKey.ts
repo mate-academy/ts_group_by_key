6073a6ad629c7426ae1adc00a2be413743ec8298
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-05 19:32:07 +0200

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,23 @@
 'use strict';
 
-// describe interfaces
+interface Item {
+  [key: string]: any;
+}
 
-export function groupByKey(array, key) {
-  // write code here;
+interface ItemsGroup<T> {
+  [key: string]: T[];
+}
+
+export function groupByKey<T extends Item>(array: T[], key: keyof T) {
+  const res: ItemsGroup<T> = {};
+
+  for (const element of array) {
+    if (!res[element[key]]) {
+      res[element[key]] = [];
+    }
+
+    res[element[key]].push(element);
+  }
+
+  return res;
 }
