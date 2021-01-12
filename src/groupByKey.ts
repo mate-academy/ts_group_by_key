d0222ba3632f71eb43cf93296435c488e4f36ecd
AndrewKreshchenko <mail2andyk@gmail.com>
2021-01-12 15:13:04 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,25 @@
 'use strict';
 
-// describe interfaces
-
-export function groupByKey(array, key) {
-  // write code here;
+interface Item {
+  [key: string]: any
 }
+
+interface ItemsGroup<T> {
+  [key: string]: T[]
+};
+
+export function groupByKey<T extends Item>(
+  items: T[], key: keyof T
+): ItemsGroup<T> {
+  const group: ItemsGroup<T> = {};
+
+  for (const item of items) {
+    if (!group.hasOwnProperty(item[key])) {
+      group[item[key]] = [];
+    }
+
+    group[item[key]].push(item);
+  }
+
+  return group;
+};
