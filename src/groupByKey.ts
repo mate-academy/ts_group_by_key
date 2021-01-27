ef0df53fa220b22b0aa3f1357efe3f8dd05d7db2
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-01-27 17:17:52 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
 
-// describe interfaces
+interface Item {
+  [key: string]: any
+}
 
-export function groupByKey(array, key) {
-  // write code here;
+interface ItemsGroup<T> {
+  [key: string]: T[]
+}
+
+export function groupByKey(array: Item[], key: string) {
+  const result = {} as ItemsGroup<Item>;
+
+  const keys = array.map(el => el[key].toString());
+
+  for (const filteredKey of keys) {
+    result[filteredKey] = [];
+
+    array.filter(el => el[key].toString() === filteredKey)
+      .forEach(el => result[filteredKey].push(el));
+  }
+
+  return result;
 }
