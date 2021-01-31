17362a347058cdf433502664625b1504f8ac6d1d
Lidiia Pryimak <lidiia.pryimak@dataart.com>
2021-01-31 18:31:06 +0200

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,29 @@
 'use strict';
 
 // describe interfaces
+interface Item {
+  [key: string]: any
+}
 
-export function groupByKey(array, key) {
-  // write code here;
+interface ItemsGroup<T> {
+  [key: string]: T[]
+}
+
+export function groupByKey<T, K extends keyof T>(
+  array: T[],
+  key: K
+): ItemsGroup<T> {
+  const result = <Item>{};
+
+  array.forEach((item) => {
+    const modifiedKey = `${item[key]}`;
+
+    if (result[modifiedKey]) {
+      result[modifiedKey].push(item);
+    } else {
+      result[modifiedKey] = [item];
+    }
+  });
+
+  return result;
 }
