8d4f8d24420c49572e963d50cc5e5dec1184f0d0
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-04-24 12:24:29 +0300

    simplified if statments

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,7 +4,7 @@ interface Item {
   [key: string]: any
 }
 
-interface ItemsGroup<T> extends Item{
+interface ItemsGroup<T> extends Item {
   [key: string]: T[]
 }
 
@@ -15,12 +15,15 @@ export function groupByKey<T, K extends keyof T>(
   for (const obj of array) {
     const resultKey = obj[key];
 
-    if ((typeof resultKey === 'string' || typeof resultKey === 'number')
-      && resultKey in result) {
-      result[resultKey].push(obj);
-    } else if (typeof resultKey === 'string' || typeof resultKey === 'number') {
-      result[resultKey] = [obj];
+    if (!(typeof resultKey === 'string' || typeof resultKey === 'number')) {
+      continue;
     }
+
+    if (!(resultKey in result)) {
+      result[resultKey] = [];
+    }
+
+    result[resultKey].push(obj);
   }
 
   return result;
