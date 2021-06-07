55dbfb4eda076c84ccdf9b492c167e3029e9b192
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-06-07 12:51:31 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,25 @@
 'use strict';
 
 // describe interfaces
+interface Item {
+  [key: string]: any
+};
 
-export function groupByKey(array, key) {
+interface ItemsGroup<T> {
+  [key: string]: T[]
+};
+
+export function groupByKey(array: Item[], key: keyof Item) {
   // write code here;
+
+  const objGroped = {} as ItemsGroup<Item>;
+
+  array.forEach(obj => {
+    if (!objGroped.hasOwnProperty(obj[key])) {
+      objGroped[obj[key]] = [];
+    }
+    objGroped[obj[key]].push(obj);
+  });
+
+  return objGroped;
 }
