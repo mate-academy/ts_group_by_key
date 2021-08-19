bc951646c3ebc43047eec36b6545e2ada1a52f69
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-19 18:44:35 +0300

    groupByKey done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,24 @@
 interface Item {
-  [key: string]: any;
+  [key: string]: any,
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface GroupedByKey<T> {
+  [key: string] : T[],
+}
+
+export function groupByKey <T extends Item>(
+  items: T[],
+  field: keyof T,
+): GroupedByKey<T> {
+  const resultObject: GroupedByKey<T> = {};
+
+  items.forEach((item: T) => {
+    if (item[field] in resultObject) {
+      resultObject[item[field]].push(item);
+    } else {
+      resultObject[item[field]] = [item];
+    }
+  });
+
+  return resultObject;
 }
