ea6738817092509cd720415552d71a54bf562cf7
SashaRudenko <for_london@ukr.net>
2021-08-20 18:32:28 +0300

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,19 @@
 interface Item {
-  [key: string]: any;
+  [key: string]: any,
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey<T extends Item, K extends keyof T>(
+  items: T[],
+  field: K,
+):
+  Record<T[K], T[]> {
+  return items.reduce(
+    (accumulator, item) => {
+      const value = item[field];
+
+      accumulator[value] = (accumulator[value] || []).concat(item);
+
+      return accumulator;
+    }, {} as Record<T[K], T[]>
+  );
 }
