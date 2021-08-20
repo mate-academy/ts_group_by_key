ad412dbe68bee7dde7f1ccfc960a22349dc52904
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-20 14:01:17 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: keyof Item) :Item {
+  const sortedArr: Item = {};
+
+  for (let i: number = 0; i < items.length; i += 1) {
+    const sortKey = items[i][field];
+
+    if (!sortedArr[sortKey]) {
+      sortedArr[sortKey] = [];
+      sortedArr[sortKey].push(items[i]);
+    } else {
+      sortedArr[sortKey].push(items[i]);
+    }
+  }
+
+  return sortedArr;
 }
