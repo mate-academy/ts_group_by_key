a4ac2253aa6575319a609b6ae6f101fb4c4ef628
Ihor Nahnybida <igor653279@gmail.com>
2021-08-21 19:49:48 +0300

    groupped by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,15 @@
-interface Item {
+export interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Array<Item>, field: string): Item {
+  return items.reduce((x, y) => {
+    const result = {
+      ...x,
+    };
+
+    result[y[field]] = items.filter((item) => item[field] === y[field]);
+
+    return result;
+  }, {});
 }
