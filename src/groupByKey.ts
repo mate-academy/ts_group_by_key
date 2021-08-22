17fec1075f7e37826c6145023f8f340d10b3f1d6
NastyaVovk <winc4ester@gmail.com>
2021-08-22 20:10:19 +0300

    initial

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: keyof Item):Item {
+  const sortedList: Item = {};
+
+  items.forEach((item: Item) => {
+    if (item[field] in sortedList) {
+      sortedList[item[field]].push(item);
+    } else {
+      sortedList[item[field]] = [item];
+    }
+  });
+
+  return sortedList;
 }
