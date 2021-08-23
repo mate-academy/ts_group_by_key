47ace1ec60864f3e4d2c6388fd9a2d14cd8e71a1
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-23 12:00:32 +0300

    the third solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,15 +2,11 @@ interface Item {
   [key: string]: any;
 }
 
-interface GroupObjByKey {
-  [key: string]: Item[];
-}
-
 export function groupByKey<T, K extends keyof Item>(
   items: T[],
   field: K,
 ): Item {
-  const resultObject: GroupObjByKey = {};
+  const resultObject: Item = {};
 
   items.forEach((item: Item) => {
     if (!resultObject[item[field]]) {
