40e441393f6afd5f7b603927aa98de8ede58608e
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-24 22:00:12 +0300

    initial solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface NewItem {
+  [key: string]: Item[];
+}
+
+export function groupByKey(items: Item[], field: keyof Item): NewItem {
+  const groupedObj: NewItem = {};
+
+  items.forEach((item) => {
+    if (!groupedObj[item[field]]) {
+      groupedObj[item[field]] = [];
+      groupedObj[item[field]].push(item);
+    } else {
+      groupedObj[item[field]].push(item);
+    }
+  });
+
+  return groupedObj;
 }
