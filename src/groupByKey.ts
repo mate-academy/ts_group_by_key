214bc252fe61ace3190405ee70be608bd554d009
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-25 20:46:21 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: string): Item {
+  const newGroup: Item = {};
+
+  items.forEach((item) => {
+    if (item[field] in newGroup) {
+      newGroup[item[field]].push(item);
+    } else {
+      newGroup[item[field]] = [item];
+    }
+  });
+
+  return newGroup;
 }
