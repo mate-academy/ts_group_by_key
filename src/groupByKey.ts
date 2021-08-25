c31532ca17c04f19c91da5361d83fb264b74ca76
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-25 19:57:09 +0300

    solution groupByKey ver 1.0

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,19 @@
+// import { group } from "console";
 interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: keyof Item): Item {
+  const sortArray: Item = {};
+
+  items.forEach((item: Item) => {
+    if (!sortArray[item[field]]) {
+      sortArray[item[field]] = [];
+      sortArray[item[field]].push(item);
+    } else {
+      sortArray[item[field]].push(item);
+    }
+  });
+
+  return sortArray;
 }
