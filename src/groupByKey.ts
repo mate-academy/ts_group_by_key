53bb99983ed7d5fe860eb08128a3fcabe062b807
Ula Dutchak <ulana.dutchak@gmail.com>
2021-08-26 13:48:56 +0300

    Group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface Groups {
+  [key: string]: Item[];
+}
+
+export function groupByKey(items: Item[], field: keyof Item): Groups {
+  const result: Groups = {};
+
+  items.forEach((item: Item) => {
+    if (item[field] in result) {
+      result[item[field]].push(item);
+    } else {
+      result[item[field]] = [item];
+    }
+  });
+
+  return result;
 }
