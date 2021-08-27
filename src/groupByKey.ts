96a30387e593beacadc3209138beb4622f274ccb
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-08-27 22:10:59 +0300

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: keyof Item):Item {
+  const resultList: Item = {};
+
+  items.forEach((item: Item) => {
+    if (item[field] in resultList) {
+      resultList[item[field]].push(item);
+    } else {
+      resultList[item[field]] = [item];
+    }
+  });
+
+  return resultList;
 }
