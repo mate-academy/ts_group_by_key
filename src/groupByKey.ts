f73e1618426745e8d9c251303db8dfc574343604
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-08-27 17:39:47 +0300

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,34 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface GroupedItems<T> {
+  [key: string]: T[]
 }
+
+export function groupByKey(
+  items: Item[], field: keyof Item,
+): GroupedItems<Item> {
+  const checked: GroupedItems<Item> = {};
+
+  items.forEach((item: Item) => {
+    if (item[field] in checked) {
+      checked[item[field]].push(item);
+    } else {
+      checked[item[field]] = [item];
+    }
+  });
+
+  return checked;
+}
+
+// type Obj1 = {
+//   x: number,
+//   y: number,
+//   z: number,
+// };
+
+// type KeyOption = keyof Obj1;
+
+// let key: KeyOption = 'x';
+
+// key = 'b';
