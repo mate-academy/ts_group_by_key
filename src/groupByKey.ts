743f83f29c301c3390986321b64bfbecfa25cac1
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-27 16:20:48 +0300

    finish them

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface FilterBy {
+  [key: string]: Item[];
+}
+
+export function groupByKey(items: Item[], field: keyof Item): FilterBy {
+  const filteredByKey: FilterBy = {};
+
+  for (let i: number = 0; i < items.length; i += 1) {
+    if (items[i][field] in filteredByKey) {
+      filteredByKey[items[i][field]].push(items[i]);
+    } else {
+      filteredByKey[items[i][field]] = [items[i]];
+    }
+  }
+
+  return filteredByKey;
 }
