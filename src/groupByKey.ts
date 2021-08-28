8e393aaec54a112132c4ccb3266d75deed193711
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-08-28 10:27:32 +0300

    groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+interface Group {
+  [key:string]: Item[];
+}
+
+export function groupByKey(items: Item[], field: keyof Item):Group {
+  const result: Group = {};
+
+  items.forEach((item) => {
+    if (item[field] in result) {
+      result[item[field]].push(item);
+    } else {
+      result[item[field]] = [item];
+    }
+  });
+
+  return result;
 }
