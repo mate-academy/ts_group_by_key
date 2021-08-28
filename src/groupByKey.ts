2b60e47f66c95e3869568e16aa81ee12cd5b7b62
unknown <hado.roman.ihorovych@gmail.com>
2021-08-28 10:38:25 +0300

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: Item[], field: string): object {
+  const finalArray: Item = {};
+
+  items.forEach((x: Item) => {
+    if (!finalArray[x[field]]) {
+      finalArray[x[field]] = [];
+      finalArray[x[field]].push(x);
+    } else {
+      finalArray[x[field]].push(x);
+    }
+  });
+
+  return finalArray;
 }
