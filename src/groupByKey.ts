4196758ce0db70c9e21ec0350e061ce41ea2b3c3
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-29 23:47:29 +0300

    v1

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
+export function groupByKey(items: Item[], field: string): object {
   // write code here;
+  const obj: object = {};
+
+  items.forEach((element: Item) => {
+    if (!obj[element[field]]) {
+      obj[element[field]] = [];
+    }
+    obj[element[field]].push(field);
+  });
+
+  return obj;
 }
