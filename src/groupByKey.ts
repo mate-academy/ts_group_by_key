8cc8200296f286d92b05256f05be7825c51972a1
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-29 22:22:15 +0300

    version 1.0

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
+export function groupByKey(items: Item[], field: keyof Item): Item {
   // write code here;
+  const result: Item = {};
+
+  items.forEach((item: Item) => {
+    if (!result[item[field]]) {
+      result[item[field]] = [];
+    }
+
+    result[item[field]].push(item);
+  });
+
+  return result;
 }
