76b899051be137645153b826fef80c5176872faf
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2021-08-30 09:25:32 +0300

    final

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+type ItemsGroup<T> = {
+  [key: string]: T[]
+};
+
+export function groupByKey(
+  items: Item[],
+  field: keyof Item,
+): ItemsGroup<Item> {
+  return items.reduce(
+    (accumulator, item) => {
+      const value = item[field];
+
+      accumulator[value] = (accumulator[value] || []).concat(item);
+
+      return accumulator;
+    },
+    {},
+  );
 }
