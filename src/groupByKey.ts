5c33e0173a616fdae10ef9d3edd89db911ae032b
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-08-30 15:19:52 +0300

    first try

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,16 +1,24 @@
 interface Item {
-  [key: string]: any;
+  [key: string]: any,
 }
 
-export function groupByKey(items: Item[], field: keyof Item) :Item {
-  const result: Item = {};
+interface GroupedByKey {
+  [key: string] : Item[],
+}
+
+export function groupByKey(
+  items: Item[],
+  field: keyof Item,
+): GroupedByKey {
+  const groupedObject: GroupedByKey = {};
 
   items.forEach((item: Item) => {
-    if (!result[item[field]]) {
-      result[item[field]] = [];
+    if (item[field] in groupedObject) {
+      groupedObject[item[field]].push(item);
+    } else {
+      groupedObject[item[field]] = [item];
     }
-    result[item[field]].push(item);
   });
 
-  return result;
+  return groupedObject;
 }
