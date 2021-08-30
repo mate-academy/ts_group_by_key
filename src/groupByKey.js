73ed67d87138ee7fb529c58828e72b4bd4a0d5c6
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-08-30 14:11:36 +0300

    fixed code review comments

---
+++ src/groupByKey.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.groupByKey = void 0;
+function groupByKey(items, field) {
+    var sortByKey = {};
+    items.forEach(function (element) {
+        if (!sortByKey[element[field]]) {
+            sortByKey[element[field]] = [];
+        }
+        sortByKey[element[field]].push(element);
+    });
+    return sortByKey;
+}
+exports.groupByKey = groupByKey;
