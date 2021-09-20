5c71f0a32f5d535472e94d4f2f2e68bba6f76302
Lenur Azizov <lenur29azizov@gmail.com>
2021-09-20 10:58:54 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,24 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items: any[], field: string): Item {
+  const newObj: Item = {};
+
+  items.forEach((element) => {
+    newObj[element[field]] = [];
+  });
+
+  items.forEach((item) => {
+    Object.keys(newObj).forEach((element) => {
+      if (typeof +element === 'number' && +element === item[field]) {
+        newObj[item[field]].push(item);
+      }
+
+      if (element === item[field]) {
+        newObj[item[field]].push(item);
+      }
+    });
+  });
+
+  return newObj;
 }
