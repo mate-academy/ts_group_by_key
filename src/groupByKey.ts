598f308106f402a1c7634a5c58bd0489f2f09004
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-06 15:46:18 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,9 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
+  const result: GroupsMap<R> = {};
+
+  return result;
 }
