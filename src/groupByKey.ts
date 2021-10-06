dfc053c215e45660202104b8a5b0a62b2de1c1c3
caelum29 <caelum29@gmail.com>
2021-10-06 15:07:40 +0300

    extends added

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,10 @@ type GroupsMap<T> = {
   [key: string]: T[]
 };
 
-export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+export function groupByKey<T, U extends keyof T>(
+  items: T[],
+  key: U,
+): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   for (let i: number = 0; i < items.length; i += 1) {
