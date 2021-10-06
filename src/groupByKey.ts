4a82154f9ed9c8ba599333772713e9c041d51095
Ivan Major <tarstarcas@gmail.com>
2021-10-06 11:33:30 +0300

    Group By Key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items, field) {
-  // write code here;
+export function groupByKey(items:Item, field: string): Item {
+  const result: Item = {};
+
+  for (let i = 0; i < items.length; i += 1) {
+    if (i === 0) {
+      result[items[i][field]] = [items[i]];
+    }
+
+    if (i > 0) {
+      if (result[items[i][field]]) {
+        result[items[i][field]].push(items[i]);
+      } else {
+        result[items[i][field]] = [items[i]];
+      }
+    }
+  }
+
+  return result;
 }
