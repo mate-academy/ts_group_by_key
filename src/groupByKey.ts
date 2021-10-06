dccfea362cb2950e6fb8e95060a3e401fcfc16d9
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-10-06 19:59:13 +0300

    group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const resultObj: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const nameKey = `${item[key]}`;
+
+    if (resultObj[nameKey]) {
+      resultObj[nameKey].push(item);
+    } else {
+      resultObj[nameKey] = [item];
+    }
+  });
+
+  return resultObj;
 }
