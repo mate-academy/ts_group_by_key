32ccbcad7271f1e7be588cba5f807b331742af8b
Oksana Onopa <onopa.kseni@gmail.com>
2021-10-06 14:30:51 +0300

    fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,9 +6,9 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groupedObjects: GroupsMap<T> = {};
 
   for (let i: number = 0; i < items.length; i + 1) {
-    const currentItem = items[i][key];
+    const currentItem = `${items[i][key]}`;
 
-    if (currentItem in groupedObjects) {
+    if (groupedObjects[currentItem]) {
       groupedObjects[currentItem].push(items[i]);
     } else {
       groupedObjects[currentItem] = [items[i]];
