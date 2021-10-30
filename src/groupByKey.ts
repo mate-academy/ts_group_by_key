89e72db27411e39656e86b66bace1d8ce287f158
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-30 18:03:37 +0300

    Task was solved

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,30 @@
+interface Book {
+  author: string;
+  country: string;
+  imageLink: string;
+  language: string;
+  pages: number;
+  title: string;
+  year: number;
+}
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey(items: Book[], key: keyof Book): GroupsMap<Book> {
   // write code here;
+  return items.reduce((object: GroupsMap<Book>, item: Book) => {
+    const groupKey: string | number = item[key];
+
+    if (object[groupKey]) {
+      object[groupKey].push(item);
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      object[groupKey] = [item];
+      // eslint-enable-next-line no-param-reassign
+    }
+
+    return object;
+  }, {});
 }
