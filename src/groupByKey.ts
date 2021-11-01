b7aafaea61c9ca5d146487b3572c1d3e9ca326d6
Alina Kostiuk <alina.hariaha@gmail.com>
2021-11-01 15:04:09 +0200

    Implement a function groupByKey.

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,24 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type InputMap<T> = {
+  [key: string]: T;
+};
+
+export function groupByKey(
+  items: InputMap<string>[], key: string,
+): GroupsMap<object> {
+  const result: GroupsMap<object> = {};
+
+  items.forEach((item) => {
+    const newKey: string = String(item[key]);
+
+    if (Object.keys(result).includes(newKey)) {
+      result[newKey].push(item);
+    } else {
+      result[newKey] = [item];
+    }
+  });
+
+  return result;
 }
