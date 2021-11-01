d95dcced235227ec5d0f6230278d911af1f6baad
Denys Anosov <anosovdenys@gmail.com>
2021-11-01 18:14:08 +0200

    added stringifiedValue variable

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,11 +6,13 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const output: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    if (String(item[key]) in output) {
-      output[String(item[key])].push(item);
+    const stringifiedValue = String(item[key]);
+
+    if (stringifiedValue in output) {
+      output[stringifiedValue].push(item);
     } else {
-      output[String(item[key])] = [];
-      output[String(item[key])].push(item);
+      output[stringifiedValue] = [];
+      output[stringifiedValue].push(item);
     }
   });
 
