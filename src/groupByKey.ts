f543cb4769a238f89481dc594bd98fdb2be66cd3
Igor Kovalev <bizzzy0@gmail.com>
2021-11-01 14:27:34 +0200

    Task was solved

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,27 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export function groupByKey(
+  items: Student[],
+  key: keyof Student,
+): GroupsMap<Student> {
+  const groupedByKey: GroupsMap<Student> = {};
+
+  items.forEach((student) => {
+    if (groupedByKey[String(student[key])] === undefined) {
+      groupedByKey[String(student[key])] = [student];
+    } else {
+      groupedByKey[String(student[key])].push(student);
+    }
+  });
+
+  return groupedByKey;
 }
