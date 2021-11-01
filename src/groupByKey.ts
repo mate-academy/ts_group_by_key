7124c4c1864664b422a85b1fadaa6362c520935f
Max <gorodtira51@gmail.com>
2021-11-01 16:37:59 +0200

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const output: GroupsMap<T> = {};
+
+  items.forEach((el): void => {
+    const valueOfKey: string = String(el[key]);
+
+    if (valueOfKey in output) {
+      output[valueOfKey].push(el);
+    } else {
+      output[valueOfKey] = [el];
+    }
+  });
+
+  return output;
 }
