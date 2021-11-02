95131a76fbc9908499551f81b6d21db744f73c12
Denys Stryzhalko <samu3lkacis@gmail.com>
2021-11-02 18:24:40 +0200

    Group by key v2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,13 +5,14 @@ type GroupsMap<T> = {
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groupedObject: GroupsMap<T> = {};
 
-  items.forEach((item) => {
-    const itemKey = item[key];
+  items.forEach((item: T) => {
+    const stringifyKey = String(item[key]);
 
-    if (!(String(itemKey) in groupedObject)) {
-      groupedObject[String(itemKey)] = [];
+    if (stringifyKey in groupedObject) {
+      groupedObject[stringifyKey].push(item);
+    } else {
+      groupedObject[stringifyKey] = [item];
     }
-    groupedObject[String(itemKey)].push(item);
   });
 
   return groupedObject;
