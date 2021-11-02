b0c8694a165d61fe52730e48600e90434d447a61
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-02 18:43:32 +0300

    Add generalized type

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,14 +5,13 @@ type GroupsMap<T> = {
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
   return items.reduce((object: GroupsMap<T>, item: T) => {
-    const groupKey: string | number = item[key];
+    const groupKey: string = String(item[key]);
 
     if (object[groupKey]) {
       object[groupKey].push(item);
     } else {
       // eslint-disable-next-line no-param-reassign
       object[groupKey] = [item];
-      // eslint-enable-next-line no-param-reassign
     }
 
     return object;
