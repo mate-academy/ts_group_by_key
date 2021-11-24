c0e2961827d2fff763bb9885dd99143d18091528
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-11-24 17:03:34 +0200

    Solution

--- src/groupByKey.test.ts
+++ src/groupByKey.test.ts
@@ -24,10 +24,10 @@ describe('Function "groupByKey"', () => {
     expect(Object.keys(groupedData))
       .toEqual(['22', '23', '24', '26']);
 
-    expect(groupedData[22].map((student) => `${student.name} ${student.age}`))
+    expect(groupedData[22].map((student: { name: any; age: any; }) => `${student.name} ${student.age}`))
       .toEqual(['Willie 22', 'Glenn 22']);
 
-    expect(groupedData[26].map((student) => `${student.name} ${student.age}`))
+    expect(groupedData[26].map((student: { name: any; age: any; }) => `${student.name} ${student.age}`))
       .toEqual(['Jessica 26']);
   });
 
--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,12 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: any[], key: string): {} {
+  const result: GroupsMap<string> = {};
+
+  for (const item of items) {
+    result[item[key]] = items.filter(obj => obj[key] === item[key]);
+  }
+
+  return result;
 }
