6793ab8c3b578f747fd1b4808a8e0628bd17b6a1
Lera Permiakova <vv.permiakova@gmail.com>
2021-12-01 15:11:50 +0200

    Group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const resObj: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyOfResObj = String(item[key]);
+
+    if (!resObj[keyOfResObj]) {
+      resObj[keyOfResObj] = [];
+    }
+
+    resObj[keyOfResObj].push(item);
+  });
+
+  return resObj;
 }
