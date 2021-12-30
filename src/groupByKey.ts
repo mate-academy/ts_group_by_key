6bcc152e572be6bea8281e41c48863d41687fde1
KHTRE <KHTREE@gmail.com>
2021-12-30 18:44:49 +0200

    works

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: object[], key: string): object {
+  const keys: string[] = [];
+  const result: GroupsMap<string> = {};
+
+  items.forEach((item) => {
+    if (!keys.includes(item[key])) {
+      keys.push(item[key]);
+    }
+  });
+
+  keys.forEach((oneKey) => {
+    result[oneKey] = items.filter((a) => a[key] === oneKey);
+  });
+
+  return result;
 }
