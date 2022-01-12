ace0cdf25676a561e25f6b8ab6f3d2879d6a66b1
Pavlo Melnyk <pashamelnyk95@gmail.com>
2022-01-12 15:01:12 +0200

    Generic improvement

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,11 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items: object[], key: string): GroupsMap<object> {
-  const groupedItems: GroupsMap<object> = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
 
   items.forEach((item) => {
-    groupedItems[item[key]] = items.filter((el) => el[key] === item[key]);
+    const itemKey = `${item[key]}`;
+
+    groupedItems[itemKey] = items.filter((el) => el[key] === item[key]);
   });
 
   return groupedItems;
