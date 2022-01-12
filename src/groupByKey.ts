e8e862a5280b1674b10bc5e40650eb55b5d5e1af
Oleksandr Leshchenko <bulochkin72@gmail.com>
2022-01-12 15:30:05 +0200

    v2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,35 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-interface Student {
-  name: string,
-  surname: string,
-  age: number,
-  married: boolean,
-  grades: number[],
-}
-
-interface Book {
-  author: string,
-  country: string,
-  imageLink: string,
-  language: string,
-  pages: number,
-  title: string,
-  year: number,
-}
-
-type Item = Student | Book;
-
-export function groupByKey(items: Item[], key: keyof Item): GroupsMap<Item> {
-  const groups: GroupsMap<Item> = {};
+export function groupByKey<I>(items: I[], key: keyof I): GroupsMap<I> {
+  const groups: GroupsMap<I> = {};
 
   items.forEach((item) => {
-    if (!Object.prototype.hasOwnProperty.call(groups, item[key])) {
-      groups[item[key]] = [];
+    if (!Object.prototype.hasOwnProperty.call(groups, `${item[key]}`)) {
+      groups[`${item[key]}`] = [];
     }
 
-    groups[item[key]].push(item);
+    groups[`${item[key]}`].push(item);
   });
 
   return groups;
