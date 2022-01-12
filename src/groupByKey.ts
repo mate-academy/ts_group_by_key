233c17a8dbc3ad84dfec439418bf6cd856efa2ec
Andrii Suprun <suprunand2016@gmail.com>
2022-01-12 21:49:04 +0100

    resolve

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const groupKey = `${item[key]}`;
+
+    if (groupedItems[groupKey] === undefined) {
+      groupedItems[groupKey] = [];
+    }
+
+    groupedItems[groupKey].push(item);
+  });
+
+  return groupedItems;
 }
