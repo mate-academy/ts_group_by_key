a599a691ae62df31568d4869ba98ac99e3ac900b
SavelovSerhii <savelov12111@gmail.com>
2022-01-13 16:47:25 +0200

    After fix

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,14 +2,14 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: GroupsMap<T>[], key: string): object {
-  const result: object = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
   let current;
 
   for (let i = 0; i < items.length; i += 1) {
-    current = items[i][key];
+    current = String(items[i][key]);
 
-    if (!Object.keys(result).includes(current.toString())) {
+    if (!result[current]) {
       result[current] = [];
     }
 
