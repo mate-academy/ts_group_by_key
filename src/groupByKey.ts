dd9959c00626980a60c973883c981711b23b3af2
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-02-09 13:57:46 +0200

    i'm really not sure about this....

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+  const values = Array.from(new Set(items.map((i) => i[key])));
+
+  values.forEach((value) => {
+    groupedItems[String(value)] = items.filter((i) => i[key] === value);
+  });
+
+  return groupedItems;
 }
