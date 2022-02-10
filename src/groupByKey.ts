a13125b03ac01fea56725f70a9dfb3514d4f1f17
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-02-10 12:56:59 +0200

    Sort by keys

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T)
+  : GroupsMap<T> {
+  const sortedObject: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const value = String(item[key]);
+
+    if (!(value in sortedObject)) {
+      sortedObject[value] = [];
+    }
+
+    sortedObject[value].push(item);
+  });
+
+  return sortedObject;
 }
