b7220f0aa8ee01b06967b637f18d2be9b65b0312
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-02-10 14:09:23 +0200

    keyof

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,23 @@
+/* eslint-disable no-restricted-syntax */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const groupObject: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const rasultKey = item[key];
+
+    if (!groupObject[rasultKey]) {
+      groupObject[rasultKey] = [];
+    }
+
+    groupObject[rasultKey].push(item);
+  });
+
+  return groupObject;
 }
