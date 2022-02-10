b8f12db5ffb446006a9122194451e3ad045e1cda
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-02-10 14:34:35 +0200

    save 2/10/2222 2:34pm

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[], key: keyof T,
+): GroupsMap<T> {
+  const resultObj: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const resultKey = `${item[key]}`;
+
+    if (!(resultKey in resultObj)) {
+      resultObj[resultKey] = [];
+    }
+
+    resultObj[resultKey].push(item);
+  });
+
+  return resultObj;
 }
