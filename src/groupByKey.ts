18bb2f61c89d061f4e1e0e9f8069b7d5a50df2e1
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2022-02-13 10:26:09 +0200

    Shamless copy

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,18 @@
+/* eslint-disable prefer-arrow-callback */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: [], key: string): GroupsMap<object> {
+  if (items.length === 0) {
+    return {};
+  }
+
+  // eslint-disable-next-line func-names
+  return items.reduce((prev, current) => {
+    // eslint-disable-next-line no-param-reassign
+    (prev[current[key]] = prev[current[key]] || []).push(current);
+
+    return prev;
+  }, {});
 }
