0797e8eda463d1ded4a8d6dc2c37c9da28036c58
Dina Samoilova <s13.dina@gmail.com>
2022-02-23 15:23:16 +0200

    after 2 review, rename var k to newKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,14 +4,14 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((res: GroupsMap<T>, obj: T) => {
-    const k = String(obj[key]);
+    const newKey = String(obj[key]);
     const result: GroupsMap<T> = { ...res };
 
-    if (!result[k]) {
-      result[k] = [];
+    if (!result[newKey]) {
+      result[newKey] = [];
     }
 
-    result[k].push(obj);
+    result[newKey].push(obj);
 
     return result;
   }, {});
