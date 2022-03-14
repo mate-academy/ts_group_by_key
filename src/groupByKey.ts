928a2dfc4f95c3a003ce831053cc0f131b992e59
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-03-15 00:18:19 +0200

    Grouping values

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const itemKey: string = item[key as keyof object];
+
+    if (!result[itemKey]) {
+      result[itemKey] = [];
+    }
+
+    result[itemKey].push(item);
+  });
+
+  return result;
 }
