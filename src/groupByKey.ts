b9ccf41cdf1de5c487586fc20cea138851fec55a
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-03-16 16:31:26 +0200

    Fixing mentor remarks

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,8 +5,8 @@ type GroupsMap<T> = {
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
-  items.forEach((item: T) => {
-    const itemKey: string = item[key];
+  items.forEach((item) => {
+    const itemKey = String(item[key]);
 
     if (!result[itemKey]) {
       result[itemKey] = [];
