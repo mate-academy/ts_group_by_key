8e76712e615f23608f2bb6052cab68abe5df6e61
krolich500grn <edu.izaytsev@gmail.com>
2022-03-18 14:16:53 +0200

    fix after review

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,11 +2,11 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const filteredByKey: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    const filter: string = item[key as keyof object];
+    const filter: string = String(item[key]);
 
     if (!filteredByKey[filter]) {
       filteredByKey[filter] = [];
