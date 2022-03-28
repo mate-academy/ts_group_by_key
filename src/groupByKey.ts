ee769c5bab57225c5a9b5df1352a9ff4aec4ab63
Roman Kovalchuk <roma220702@gmail.com>
2022-03-28 17:47:20 +0300

    some message

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,32 @@
+// import { type } from "os";
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+interface Books {
+  author: string,
+  country: string,
+  imageLink: string,
+  language: string,
+  pages: number,
+  title: string,
+  year: number
+}
+
+type Key = 'author' | 'country' | 'imageLink' |
+'language' | 'pages' | 'title' | 'year';
+
+export function groupByKey(items: Books[], key: Key): GroupsMap<Books> {
+  const result: GroupsMap<Books> = {};
+
+  const mass: any[] = [];
+
+  items.forEach((element: Books) => mass.push(element[key]));
+
+  mass.forEach((element) => {
+    result[element] = items.filter((el: Books) => el[key] === element);
+  });
+
+  return result;
 }
