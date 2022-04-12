0396aee0e151d0fb735e36543671b7e8f90c025e
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-04-12 16:43:07 +0300

    The solution was implemented

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,25 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type MainTypesUnion = string | number;
+
+type ItemObj = {
+  [key: string]: MainTypesUnion;
+};
+
+export function groupByKey(items: ItemObj[], key: string): GroupsMap<ItemObj> {
+  const allKeysArray: MainTypesUnion[] = items.map((item) => item[key]);
+
+  const uniqKeysArray: MainTypesUnion[]
+    = allKeysArray.filter((elem, index) => allKeysArray
+      .indexOf(elem) === index);
+
+  const grouppedItems: GroupsMap<ItemObj> = {};
+
+  for (let i: number = 0; i < uniqKeysArray.length; i += 1) {
+    grouppedItems[uniqKeysArray[i]]
+      = items.filter((item) => item[key] === uniqKeysArray[i]);
+  }
+
+  return grouppedItems;
 }
