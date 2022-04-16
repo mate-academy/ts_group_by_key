ba20ad7608489995f02d7a3e13ce66dd1601084a
unknown <xpromt@gmail.com>
2022-04-16 13:09:43 +0300

    Result

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const resGroup: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const keyG: string = item[key as keyof object];
+
+    if (resGroup[keyG]) {
+      resGroup[keyG].push(item);
+    } else {
+      resGroup[keyG] = [item];
+    }
+  });
+
+  return resGroup;
 }
