dd645050234010bd6ad00cd2df729a7fe5f92c9a
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-21 13:16:16 +0300

    GroupByKey implemented

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const groups: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    if (groups[item[key]]) {
+      groups[item[key]].push(item);
+    } else {
+      groups[item[key]] = [item];
+    }
+  });
+
+  return groups;
 }
