0740d6737a7666233444a61c01537112999b9932
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-04-25 13:43:00 +0300

    GroupByKey function was created

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((elem) => {
+    const prop: any = elem[key];
+
+    if (prop in result) {
+      result[prop].push(elem);
+    } else {
+      result[prop] = [elem];
+    }
+  });
+
+  return result;
 }
