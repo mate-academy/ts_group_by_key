25b3da2137c32c2076bae8e282fff5c3a2677b08
MariaBondar <marybondar777@gmail.com>
2022-04-26 14:49:37 +0200

    develop

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const object: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const currentKey = String(item[key]);
+
+    if (!object[currentKey]) {
+      object[currentKey] = [];
+    }
+
+    object[currentKey].push(item);
+  });
+
+  return object;
 }
