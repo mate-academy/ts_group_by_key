ed755c3da43238bf8f3f2ed1ec867bc245f10771
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-26 13:15:38 +0300

    types fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,15 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groups: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    if (groups[item[key]]) {
-      groups[item[key]].push(item);
-    } else {
-      groups[item[key]] = [item];
-    }
+    const groupKey = String(item[key]);
+
+    groups[groupKey] = [...(groups[groupKey] || []), item];
   });
 
   return groups;
