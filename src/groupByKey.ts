9a1229bc3b149509285e1c58d2d4a7ab314ca9b7
Misha Kravtsov <aniahucm@gmail.com>
2022-04-26 13:46:26 +0300

    save file lines, forEach instead for

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,22 +2,21 @@ type GroupsMap<T> = {
   [key: string]: Array<T>;
 };
 
-export function groupByKey<T>(items: Array<T>,
-  key: string): GroupsMap<T> {
+export function groupByKey<T>(
+  items: Array<T>,
+  key: string,
+): GroupsMap<T> {
   const outObj: GroupsMap<T> = {};
 
-  let tempKey: string | number;
-
-  for (let i: number = 0; i < items.length;) {
-    tempKey = items[i][key];
+  items.forEach((item: T) => {
+    const tempKey: string | number = item[key];
 
     if (!Object.prototype.hasOwnProperty.call(outObj, tempKey)) {
       outObj[tempKey] = [];
     }
-    outObj[tempKey].push(items[i]);
 
-    i += 1;
-  }
+    outObj[tempKey].push(item);
+  });
 
   return outObj;
 }
