6838f2f88a6691be1debb58602275e3cb7377a5f
WyLNdOoa <GreyIl@protonmail.com>
2022-04-27 18:02:40 +0300

    task done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,3 +1,4 @@
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
@@ -21,22 +22,3 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
 
   return groupsMap;
 }
-
-// export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-//   const groupsMap: GroupsMap<T> = {};
-
-//   for (const item of items) {
-//     const keyValue = item[key];
-
-//     if (typeof keyValue !== "string") {
-//       throw new Error("Value of key for grouping should have type string");
-//     }
-//     if (keyValue in groupsMap) {
-//       groupsMap[keyValue].push(item);
-//     } else {
-//       groupsMap[keyValue] = [item];
-//     }
-//   }
-
-//   return groupsMap;
-// }
