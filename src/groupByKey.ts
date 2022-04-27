bcc859263bebcaececb22399f9b0e98dc18737a2
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-27 19:25:21 +0700

    variable added but linter don't accept without String

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,14 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   for (let i: number = 0; i < items.length; i += 1) {
-    if (!result[items[i][key].toString()]) {
-      result[items[i][key].toString()] = [];
+    const item: string = String(items[i][key]);
+
+    if (!result[item]) {
+      result[item] = [];
     }
-    result[items[i][key].toString()].push(items[i]);
+    result[item].push(items[i]);
   }
 
   return result;
