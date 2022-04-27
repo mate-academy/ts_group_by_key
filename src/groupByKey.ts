05e36e9bbcd6d795c8273fa04d351b90e7cae0f6
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-04-27 15:08:34 +0300

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce<GroupsMap<T>>((groupsMap, item) => {
+    const groups = groupsMap;
+    const keyValue = item[key];
+
+    if (typeof keyValue !== 'string' && typeof keyValue !== 'number') {
+      throw new Error('Value of key should be string or number');
+    }
+
+    if (keyValue in groups) {
+      groups[keyValue].push(item);
+    } else {
+      groups[keyValue] = [item];
+    }
+
+    return groupsMap;
+  }, {});
 }
