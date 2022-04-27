44342316dc146397185a8d0d43f919a2e739e9fd
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-04-27 21:17:40 +0300

    fix

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,18 +4,18 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce<GroupsMap<T>>((groupsMap, item) => {
-    const groups = groupsMap;
     const keyValue = item[key];
 
     if (typeof keyValue !== 'string' && typeof keyValue !== 'number') {
       throw new Error('Value of key should be string or number');
     }
 
-    if (keyValue in groups) {
-      groups[keyValue].push(item);
-    } else {
-      groups[keyValue] = [item];
+    if (!(keyValue in groupsMap)) {
+      // eslint-disable-next-line no-param-reassign
+      groupsMap[keyValue] = [];
     }
+
+    groupsMap[keyValue].push(item);
 
     return groupsMap;
   }, {});
