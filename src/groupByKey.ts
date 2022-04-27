515d43d463d957f092808dc6518fcc556bd68e2c
Ihor Mural <igor201278789887000@gmail.com>
2022-04-27 14:44:38 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,20 @@
-type GroupsMap<T> = {
-  [key: string]: T[];
-};
+type GroupsMap<T> = Record<number | string, T[]>;
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce<GroupsMap<T>>((groupsMap, item) => {
+    const grpmap = groupsMap;
+    const keyValue = item[key];
+
+    if (typeof keyValue !== 'string' && typeof keyValue !== 'number') {
+      throw new Error('Value of key for grouping should have type string');
+    }
+
+    if (keyValue in grpmap) {
+      grpmap[keyValue].push(item);
+    } else {
+      grpmap[keyValue] = [item];
+    }
+
+    return groupsMap;
+  }, {});
 }
