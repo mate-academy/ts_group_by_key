0a948bb62aff143841d0642f9a3a11b289015be4
WyLNdOoa <GreyIl@protonmail.com>
2022-04-27 17:58:43 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,41 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupsMap: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyValue = item[key];
+
+    if (typeof keyValue !== 'string' && typeof keyValue !== 'number') {
+      throw new Error('Invalid data');
+    }
+
+    if (keyValue in groupsMap) {
+      groupsMap[keyValue].push(item);
+    } else {
+      groupsMap[keyValue] = [item];
+    }
+  });
+
+  return groupsMap;
 }
+
+// export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+//   const groupsMap: GroupsMap<T> = {};
+
+//   for (const item of items) {
+//     const keyValue = item[key];
+
+//     if (typeof keyValue !== "string") {
+//       throw new Error("Value of key for grouping should have type string");
+//     }
+//     if (keyValue in groupsMap) {
+//       groupsMap[keyValue].push(item);
+//     } else {
+//       groupsMap[keyValue] = [item];
+//     }
+//   }
+
+//   return groupsMap;
+// }
