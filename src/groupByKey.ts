bee748a14abdae6cbbc19a9ae3e99560ccdcc132
unknown <avicostov2@gmail.com>
2022-05-06 19:52:34 +0200

    solut

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const newObject: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const newKey: string = String(item[key]);
+
+    if (!(newKey in newObject)) {
+      newObject[newKey] = [];
+    }
+
+    newObject[newKey].push(item);
+  });
+
+  return newObject;
 }
