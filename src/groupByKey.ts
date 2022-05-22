7995ae7b6d7340f53cdc772df5cca9d112173a78
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-22 12:55:56 +0300

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const obj: GroupsMap<T> = {};
+
+  items.forEach((el: T) => {
+    const myKey = String(el[key]);
+
+    // obj[myKey] ? obj[myKey].push(el) : obj[myKey] = [el];
+    // =======    why ternar not work ???
+
+    if (obj[myKey]) {
+      obj[myKey].push(el);
+    } else {
+      obj[myKey] = [el];
+    }
+  });
+
+  return obj;
 }
