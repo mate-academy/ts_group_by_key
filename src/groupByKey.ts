c06f46cd3306b9133d637a930a87568d84ca4b09
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-05-23 16:20:29 +0300

    droup by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const groupedByKey: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const currentKey = String(item[key]);
+
+    if (!groupedByKey[currentKey]) {
+      groupedByKey[currentKey] = [];
+    }
+
+    groupedByKey[currentKey].push(item);
+  });
+
+  return groupedByKey;
 }
