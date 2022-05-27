a770834595cf8cc66ffd56684e3d7bbba0122340
Dima Cherednyk <dicherednyk@gmail.com>
2022-05-27 20:22:15 +0300

    commit message

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export
+function groupByKey(items: object[], key: keyof object): object {
+  const callback = (prev: object, user: object): GroupsMap<object> => {
+    return {
+      ...prev,
+      [user[key]]: items.filter((x) => x[key] === user[key]),
+    };
+  };
+
+  const obj = items.reduce(callback, {});
+
+  return obj;
 }
