8ca05f540e25718e5e84ca46f2d96c454ec9c6d5
Dima Cherednyk <dicherednyk@gmail.com>
2022-05-29 20:18:02 +0300

    commit message

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,16 +2,28 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export
-function groupByKey(items: object[], key: keyof object): object {
-  const callback = (prev: object, user: object): GroupsMap<object> => {
-    return {
-      ...prev,
-      [user[key]]: items.filter((x) => x[key] === user[key]),
-    };
-  };
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
 
-  const obj = items.reduce(callback, {});
+  items.forEach((element: T) => {
+    const index = String(element[key]);
 
-  return obj;
+    result[index] = items.filter((x) => x[key] === element[key]);
+  });
+
+  return result;
 }
+
+// or with reduce method
+
+// export
+// function groupByKey<T>(items: T[], key: keyof object): GroupsMap<T> {
+//   function callback(prev: GroupsMap<T>, user: T): GroupsMap<T> {
+//     return {
+//       ...prev,
+//       [user[key]]: items.filter((x: T) => x[key] === user[key]),
+//     };
+//   }
+
+//   return items.reduce<GroupsMap<T>>(callback, {} as GroupsMap<T>);
+// }
