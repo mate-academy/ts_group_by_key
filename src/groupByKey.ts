131df50bb6f748c30a175324c72d2e79a3b8f02b
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-06-02 22:38:49 +0300

    last task ts

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const groupObj: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const value = String(item[key]);
+
+    if (!groupObj[value]) {
+      groupObj[value] = [];
+    }
+
+    groupObj[value].push(item);
+  });
+
+  return groupObj;
 }
