1f942f74cb1f7df81be9e005cb8d044f51c4be66
Illia Vodonis <illavodonis@gmail.com>
2022-06-30 13:51:10 +0300

    develop

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const startValue: GroupsMap<T> = {};
+
+  const resultGroups = items.reduce((result, item) => {
+    const mappingObj = { ...result };
+
+    if (!mappingObj[`${item[key]}`]) {
+      mappingObj[`${item[key]}`] = [];
+    }
+
+    mappingObj[`${item[key]}`].push(item);
+
+    return { ...mappingObj };
+  }, startValue);
+
+  return resultGroups;
 }
