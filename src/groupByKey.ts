20c22db4da30853f36c2a55ba8760eaa30c064b0
mira1zu <mira1zu733@gmail.com>
2022-06-30 11:33:16 +0200

    Fixes

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,17 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key): GroupsMap<object> {
-  const groupedObj: GroupsMap<object> = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedObj: GroupsMap<T> = {};
 
   items.forEach((object) => {
-    const value = object[key];
+    const value = String(object[key]);
 
-    if (groupedObj[value]) {
-      groupedObj[value].push(object);
-    } else {
-      groupedObj[value] = [object];
+    if (!groupedObj[value]) {
+      groupedObj[value] = [];
     }
+
+    groupedObj[value].push(object);
   });
 
   return groupedObj;
