a7b6878fa1b7fb5f455fc8134eaae8c7c3bf7385
SAMAEV <andreysamaev@gmail.com>
2022-06-30 12:34:05 +0300

    groupedBy

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const value = String(item[key]);
+
+    if (!result[value]) {
+      result[value] = [];
+    }
+
+    result[value].push(item);
+  });
+
+  return result;
 }
