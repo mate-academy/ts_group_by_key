4388c7da777ea996f5265c4cbceba0399ce74bf6
SpaceHook <maxdumiak39@gmail.com>
2022-06-30 12:23:52 +0300

    ready

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedResult: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const value = String(item[key]);
+
+    if (!groupedResult[value]) {
+      groupedResult[value] = [item];
+    } else {
+      groupedResult[value].push(item);
+    }
+  });
+
+  return groupedResult;
 }
