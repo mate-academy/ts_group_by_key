50b3dff0a50240036699cbbe2a9b637ebb42a497
Pavlo <Pmoroz2004@gmail.com>
2022-06-30 11:47:55 +0300

    fixed mistakes

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,16 +1,14 @@
-type GroupsMap = {
-  [key: string]: [];
+type GroupsMap<T> = {
+  [key: string]: T[];
 };
 
-export function groupByKey(items: GroupsMap[], key: keyof GroupsMap): object {
-  const groups = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
 
   for (let i = 0; i < items.length; i += 1) {
-    const option = items[i][key];
-
-    groups[option] = items
-      .filter((currentOption: GroupsMap) => currentOption[key] === option);
+    result[String(items[i][key])] = items
+      .filter((currentOption: T) => currentOption[key] === items[i][key]);
   }
 
-  return groups;
+  return result;
 }
