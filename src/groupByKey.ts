d187a5318ce168ef91ecdde5fc825c239c89e07b
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-07-21 18:28:18 +0300

    firstCommit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const obj: GroupsMap<T> = {};
+
+  items.forEach((element) => {
+    const x = String(element[key]);
+
+    if (!obj[x]) {
+      obj[x] = [];
+    }
+
+    obj[x].push(element);
+  });
+
+  return obj;
 }
