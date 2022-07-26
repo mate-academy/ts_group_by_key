f94758663d770838a3e96f2fabd0d5b7c1484865
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-07-26 19:27:42 +0300

    Group by Key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: string[],
+  key: string): GroupsMap<T> {
   // write code here;
+  const obj = {};
+
+  items.forEach((i) => {
+    if (!obj[i[key]]) {
+      obj[i[key]] = [];
+    }
+
+    obj[i[key]].push(i);
+  });
+
+  return obj;
 }
