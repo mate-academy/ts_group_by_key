983aaea7650ee3328756bbedaaf0da8773ddd899
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-07-28 16:22:01 +0300

    task is reay

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,22 @@
+// import { booleanLiteral } from "@babel/types";
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const byGroup: GroupsMap<T> = {};
+
+  items.forEach((element) => {
+    const elKey = String(element[key]);
+
+    if (!Object.prototype.hasOwnProperty.call(byGroup, elKey)) {
+      byGroup[elKey] = [];
+    }
+
+    byGroup[elKey].push(element);
+  });
+
+  return byGroup;
 }
