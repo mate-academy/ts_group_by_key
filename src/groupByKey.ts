dd328a0acded6c28a8bf43954d5ec320f8c9a888
Dmitriy Teteryn <106395517+DmitriyTeteryn@users.noreply.github.com>
2022-07-29 18:58:47 +0300

    Create groupByKey.ts

---
+++ src/groupByKey.ts
@@ -0,0 +1,19 @@
+type GroupsMap<T> = {
+  [key: string]: T[];
+};
+
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const res: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const resKey = String(item[key]);
+
+    if (!res[resKey]) {
+      res[resKey] = [];
+    }
+
+    res[resKey].push(item);
+  });
+
+  return res;
+}
