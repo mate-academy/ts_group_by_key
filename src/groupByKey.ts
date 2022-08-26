34ecef62731cbf7a29bd078b27e73062cb707ce7
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-26 16:59:02 +0300

    Fixed item value to be converted to string as object key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,7 +6,7 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const grouped: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    const itemValue: any = item[key];
+    const itemValue: string = String(item[key]);
 
     if (!Object.prototype.hasOwnProperty.call(grouped, itemValue)) {
       grouped[itemValue] = [];
