c0c6bae84480c32eb9eb89349805e23bc9509bdd
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-08-27 15:07:15 +0300

    Draft

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,28 @@
+// import { forEachChild } from "typescript";
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type Item = {
+  [key: string]: string | number | boolean | number[];
+};
+
+type T = Item;
+
+export function groupByKey(items: T[], key: string): GroupsMap<T> {
+  const group: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const groupKey = `${item[key]}`;
+
+    if (Object.prototype.hasOwnProperty.call(group, groupKey)) {
+      group[groupKey].push(item);
+    } else {
+      group[groupKey] = [];
+      group[groupKey].push(item);
+    }
+  });
+
+  return group;
 }
