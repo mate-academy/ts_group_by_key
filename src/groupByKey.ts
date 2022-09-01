08b89735808d5ac95675a87cd890ec700582b966
Andrii Stiebalov <stiebalov@gmail.com>
2022-09-01 17:20:03 +0300

    function group by key was added

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<F>(items: F[], key: string): GroupsMap<F> {
+  const sort = items.reduce((prev, elem: F): GroupsMap<F> => {
+    const obj = { ...prev };
+
+    if (!prev[elem[key]]) {
+      obj[elem[key]] = [];
+    }
+
+    obj[elem[key]].push(elem);
+
+    return obj;
+  }, {});
+
+  return sort;
 }
