e99a54320fe40c74ae05e645a98bdb192b3ec77d
Yevhen Veselov <veselovyevhen@gmail.com>
2022-09-23 12:57:34 +0300

    1st commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const groupBy = String(item[key]);
+
+    if (!result[groupBy]) {
+      result[groupBy] = [];
+    }
+
+    result[groupBy].push(item);
+  });
+
+  return result;
 }
