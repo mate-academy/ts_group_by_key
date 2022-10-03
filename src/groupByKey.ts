1b39ab8a0fcf33712d2c67e358136d5c80449ac3
Hovera-Liubomyr <ljusuk@gmail.com>
2022-10-03 10:47:49 +0300

    group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,20 @@
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const mas: GroupsMap<T> = {};
+
+  items.forEach((el) => {
+    const itemKey = `${el[key]}`;
+
+    if (!mas[itemKey]) {
+      mas[itemKey] = [];
+    }
+
+    mas[itemKey].push(el);
+  });
+
+  return mas;
 }
