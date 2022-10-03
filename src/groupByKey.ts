4b44e21dde2b7e81ff120fd966c72689fef5c6d8
Killdex <kdcompany2018@gmail.com>
2022-10-03 21:47:17 +0300

    group funcrion has been created

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: [], key: string): GroupsMap<T> {
+  const copyItems = [...items];
+
+  const outputObject = copyItems.reduce((prev: GroupsMap<T>, object) => {
+    return {
+      ...prev,
+      [object[key]]: copyItems.filter((item) => item[key] === object[key]),
+    };
+  }, {});
+
+  return outputObject;
 }
