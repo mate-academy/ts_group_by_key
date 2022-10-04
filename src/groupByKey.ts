dfc4b334eea34508d89e8e7f1cea023d162b2b95
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-10-04 20:25:40 +0300

    groupByKeys was implemented

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    groupedItems[`${item[key]}`] = [];
+  });
+
+  items.forEach((item: T) => {
+    groupedItems[`${item[key]}`].push(item);
+  });
+
+  return groupedItems;
 }
