6b8322e977d3520e09f0efa2f902f60a78e45e51
Dima Romanenko <demigod177712@gmail.com>
2022-10-05 19:59:20 +0300

    type fixes

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,15 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: any[], key: keyof T): GroupsMap<object> {
-  const groupped: GroupsMap<object> = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupped: GroupsMap<T> = {};
 
   items.forEach((item) => {
-    if (!groupped[item[key]]) {
-      groupped[item[key]] = [];
+    if (!groupped[String(item[key])]) {
+      groupped[String(item[key])] = [];
     }
 
-    groupped[item[key]].push(item);
+    groupped[String(item[key])].push(item);
   });
 
   return groupped;
