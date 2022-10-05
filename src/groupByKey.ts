441e320d405db70b2116361b64049d8aed1d334d
Vlad Malinin <vladmalinin1999@gmail.com>
2022-10-05 18:41:26 +0300

    Changed on one cycle

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,10 +6,9 @@ export function groupByKey<T>(items:T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   for (let i = 0; i < items.length; i += 1) {
-    result[String(items[i][key])] = [];
-  }
-
-  for (let i = 0; i < items.length; i += 1) {
+    if (!result[String(items[i][key])]) {
+      result[String(items[i][key])] = [];
+    }
     result[String(items[i][key])].push(items[i]);
   }
 
