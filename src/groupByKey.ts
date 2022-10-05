0e78e5f5931cba076bae5417820fc2fa5e72dd82
Dima Romanenko <demigod177712@gmail.com>
2022-10-05 16:48:38 +0300

    Groupped

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: any[], key: keyof T): GroupsMap<object> {
+  const groupped: GroupsMap<object> = {};
+
+  items.forEach((item) => {
+    if (!groupped[item[key]]) {
+      groupped[item[key]] = [];
+    }
+
+    groupped[item[key]].push(item);
+  });
+
+  return groupped;
 }
