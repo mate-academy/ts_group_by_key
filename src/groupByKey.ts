373e38186e213f199c6e26d4ff623988114010da
Datsenko Mariia <datsenko.mate@gmail.com>
2022-10-05 20:06:47 +0300

    if case has been fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,11 +6,11 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const grouped: GroupsMap<T> = {};
 
   for (let i = 0; i < items.length; i += 1) {
-    if (grouped[String(items[i][key])]) {
-      grouped[String(items[i][key])].push(items[i]);
-    } else {
+    if (!grouped[String(items[i][key])]) {
       grouped[String(items[i][key])] = [];
     }
+
+    grouped[String(items[i][key])].push(items[i]);
   }
 
   return grouped;
