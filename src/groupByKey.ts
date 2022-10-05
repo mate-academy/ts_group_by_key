9904f9fa7b6a6aed0a31d1ce44492cc16cccf14a
Oleh Bazyla <olegbazila@gmail.com>
2022-10-05 15:38:32 +0300

    develop

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((acc: GroupsMap<T>, curr: T) => {
+    if (acc[String(curr[key])]) {
+      acc[String(curr[key])] = [];
+    }
+    acc[String(curr[key])].push(curr);
+
+    return acc;
+  }, {});
 }
