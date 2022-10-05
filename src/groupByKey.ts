f4ed41aa28b7832e903234fd869183ff667b7de6
Serhii Hluhovskyi <glugovsky@proton.me>
2022-10-05 22:01:54 +0300

    Added groupByKey function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((total: GroupsMap<T>, currentItem: T) => {
+    const currentKey: string = currentItem[key].toString();
+    const groups: GroupsMap<T> = total;
+
+    if (!groups[currentKey]) {
+      groups[currentKey] = [];
+    }
+
+    groups[currentKey].push(currentItem);
+
+    return groups;
+  }, {});
 }
