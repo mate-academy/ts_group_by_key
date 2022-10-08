8b08a7bdcb034c0eae44fb5e5ee0b8b747f7abc7
Dima Romanenko <demigod177712@gmail.com>
2022-10-08 11:11:43 +0300

    variable created

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,11 +6,13 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groupped: GroupsMap<T> = {};
 
   items.forEach((item) => {
-    if (!groupped[String(item[key])]) {
-      groupped[String(item[key])] = [];
+    const itemKey = String(item[key]);
+
+    if (!groupped[itemKey]) {
+      groupped[itemKey] = [];
     }
 
-    groupped[String(item[key])].push(item);
+    groupped[itemKey].push(item);
   });
 
   return groupped;
