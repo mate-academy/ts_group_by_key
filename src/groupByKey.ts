4579e022d44533b4d2b05fcdeecb482efca39427
Killdex <kdcompany2018@gmail.com>
2022-10-08 10:50:10 +0300

    implemented with one array iteration

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,10 +3,17 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: [], key: keyof T): GroupsMap<T> {
-  return items.reduce((prev: GroupsMap<T>, object) => {
-    return {
-      ...prev,
-      [object[key]]: items.filter((item) => item[key] === object[key]),
-    };
-  }, {});
+  const group: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const currentKey = item[key];
+
+    if (!group[currentKey]) {
+      group[currentKey] = [];
+    }
+
+    group[currentKey].push(item);
+  });
+
+  return group;
 }
