ab44513b4d6f0471beffa4012871642dadab7d29
Andriy Andrieiev <proph7000@gmail.com>
2022-10-10 13:56:01 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type Prop = 'string' | 'number';
+
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  for (let i = 0; i < items.length; i += 1) {
+    const obj: T = items[i];
+    const keyObj = obj[key] as Prop;
+
+    if (!result[keyObj]) {
+      result[keyObj] = items.filter((item) => item[key] === keyObj);
+    }
+  }
+
+  return result;
 }
