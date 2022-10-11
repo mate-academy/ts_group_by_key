f5cd4104bb314ca9088c7705428a88dfffd7980c
RomaVarkov <varkovroma@gmail.com>
2022-10-11 17:49:45 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,10 +6,9 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const grouped: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    grouped[String(item[key])] = [];
-  });
-
-  items.forEach((item: T) => {
+    if (grouped[String(item[key])] === undefined) {
+      grouped[String(item[key])] = [];
+    }
     grouped[String(item[key])].push(item);
   });
 
