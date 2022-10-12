e329ed063d165a2d827d04a638df6eff723d8d42
fatihsamur <a.fatihsamur@gmail.com>
2022-10-13 01:07:00 +0300

    ref

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,16 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(
-  items: GroupsMap<T>[],
-  key: keyof GroupsMap<T>,
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
 ): GroupsMap<T> {
+  // to get unique groups array from given items
   const groups = items
     .map((item) => item[key])
     .filter((item, index, arr) => arr.indexOf(item) === index);
 
-  const groupedList = {};
+  const groupedList: GroupsMap<T> = {};
 
+  // assign the array to the matching group key
   groups.forEach((group) => {
     groupedList[group] = items.filter((item) => item[key] === group);
   });
