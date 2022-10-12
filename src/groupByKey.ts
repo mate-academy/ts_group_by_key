083f33e4c482ec4035c45b2bf29be716c0476fb8
fatihsamur <a.fatihsamur@gmail.com>
2022-10-12 23:54:47 +0300

    linter is not happy

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(
+  items: GroupsMap<T>[],
+  key: keyof GroupsMap<T>,
+): GroupsMap<T> {
+  const groups = items
+    .map((item) => item[key])
+    .filter((item, index, arr) => arr.indexOf(item) === index);
+
+  const groupedList = {};
+
+  groups.forEach((group) => {
+    groupedList[group] = items.filter((item) => item[key] === group);
+  });
+
+  return groupedList;
 }
