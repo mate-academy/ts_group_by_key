9e09e28c45c0fb2c43ac9e25a5933a392663bc0a
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-10-13 09:22:34 +0530

    sol

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const groupedList: GroupsMap<T> = {};
+
+  // to get unique groups array from given items
+  items
+    .map((item) => item[key])
+    .filter((item, index, arr) => arr.indexOf(item) === index)
+    .forEach((group) => {
+      groupedList[String(group)] = items.filter((item) => item[key] === group);
+    });
+
+  return groupedList;
 }
