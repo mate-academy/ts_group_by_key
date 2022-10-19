41f6133ba219be42e851a723c7a0dd4e4d036d3d
Bibicutu <pauleugen@gmail.com>
2022-10-20 01:33:48 +0300

    First commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedByKey: GroupsMap<T> = {};
+  const keysInGrouped: any[] = [];
+
+  items.forEach((item) => {
+    if (!keysInGrouped.includes(item[key])) {
+      keysInGrouped.push(item[key]);
+    }
+  });
+
+  keysInGrouped.forEach((group) => {
+    groupedByKey[group] = items.filter((element) => element[key] === group);
+  });
+
+  return groupedByKey;
 }
