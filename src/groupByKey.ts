5f56517094540a4d1f52849cbb4d52499867abab
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-10-27 17:55:29 +0300

    TS group by key task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(
+  items: Array<object>, key: keyof object,
+): GroupsMap<object> {
+  const groupedItems: GroupsMap<object> = {};
+  const keysValues: string[] = [];
+
+  items.forEach((value: object) => {
+    if (!keysValues.includes(value[key])) {
+      keysValues.push(value[key]);
+    }
+  });
+
+  keysValues.forEach((value: string) => {
+    groupedItems[value] = items.filter((item: object) => item[key] === value);
+  });
+
+  return groupedItems;
 }
