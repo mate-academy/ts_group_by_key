0ce5f675ecf7b0a8e538fbb1d4fb46aa64ed845f
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-11-05 23:12:12 +0200

    Implement sortBy function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,14 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
+  const condition: R[keyof R][] = items.map((item: R) => item[key]);
+
+  const groups: GroupsMap<R> = {};
+
+  condition.forEach((el: R[keyof R]) => {
+    groups[el as string] = items.filter((item: R) => item[key] === el);
+  });
+
+  return groups;
 }
