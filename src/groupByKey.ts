a09c4d2e17d0679d0748949b4d28f1633ca60549
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-11-07 14:43:34 +0200

    Refactor code without nesting loops

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,12 +3,16 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
-  const condition: R[keyof R][] = items.map((item: R) => item[key]);
-
   const groups: GroupsMap<R> = {};
 
-  condition.forEach((el: R[keyof R]) => {
-    groups[el as string] = items.filter((item: R) => item[key] === el);
+  items.forEach((item: R) => {
+    const condition: R[keyof R] = item[key];
+
+    if (!groups[condition as string]) {
+      groups[condition as string] = [];
+    }
+
+    groups[condition as string].push(item);
   });
 
   return groups;
