1d4f9c26d35970b6c6a0f3e0b9a545047ac031d3
Iryna Lelyk <irunochka771@gmail.com>
2022-11-11 09:59:40 +0200

    Group by key was added

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: object[], key: string): GroupsMap<object> {
+  const groupedObjects: GroupsMap<object> = {};
+
+  items.forEach((item: {[key: string]: any}) => {
+    if (!Object.prototype.hasOwnProperty.call(groupedObjects, item[key])) {
+      groupedObjects[item[key]] = [];
+    }
+
+    groupedObjects[item[key]].push(item);
+  });
+
+  return groupedObjects;
 }
