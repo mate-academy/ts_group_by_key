3daca6153870c97d942dd0fabdd4f29f45413b73
Vadym Baranov <vadym.baranov@hotmail.com>
2022-11-12 15:57:53 +0200

    Items grouped

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedByValue: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const value = String(item[key]);
+
+    if (groupedByValue[value]) {
+      groupedByValue[value].push(item);
+    } else {
+      groupedByValue[value] = [item];
+    }
+  });
+
+  return groupedByValue;
 }
