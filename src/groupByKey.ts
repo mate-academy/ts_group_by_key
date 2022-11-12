005fe0ace6cb4fc25ba7f23ba99e832c18107891
Svitlana Yudina <osbka31@gmail.com>
2022-11-12 21:42:06 +0200

    first commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedObj: GroupsMap<T> = {};
+
+  for (let i: number = 0; i < items.length; i += 1) {
+    const keyValue = String(items[i][key]);
+
+    if (!groupedObj[keyValue]) {
+      groupedObj[keyValue] = [];
+    }
+    groupedObj[keyValue].push(items[i]);
+  }
+
+  return groupedObj;
 }
