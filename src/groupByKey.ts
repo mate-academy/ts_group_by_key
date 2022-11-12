51af240c732908811f18fe211ea7c2ce46da5ad3
Masha <teenderrr@gmail.com>
2022-11-12 17:22:23 +0200

    fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,21 +4,21 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  let obj: GroupsMap<T> = {};
+  let groupedByKey: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
     const value = item[key];
 
     if (typeof value === 'string' || typeof value === 'number') {
-      if (!obj[value]) {
-        obj = {
-          ...obj,
+      if (!groupedByKey[value]) {
+        groupedByKey = {
+          ...groupedByKey,
           [value]: [],
         };
       }
-      obj[value].push(item);
+      groupedByKey[value].push(item);
     }
   });
 
-  return obj;
+  return groupedByKey;
 }
