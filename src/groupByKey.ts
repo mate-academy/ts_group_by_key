1615872ff94b1ddde1cd3ce8d30a59a71d4a1d98
Artem66 <melodpi@gmail.com>
2022-11-12 13:57:47 +0000

    implemented groupByKey function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items:[], key: string): GroupsMap<object> {
+  const result: GroupsMap<object> = {};
+
+  items.forEach((item: {[key: string]: any}): void => {
+    if (!Object.prototype.hasOwnProperty.call(result, item[key])) {
+      result[item[key]] = [item];
+    } else {
+      result[item[key]].push(item);
+    }
+  });
+
+  return result;
 }
