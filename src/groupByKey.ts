d4b9a902473906346c226309088ca35252f7ff89
Spirit <baolt.king@gmail.com>
2022-11-13 23:44:29 +0200

    complete

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T) :GroupsMap<T> {
+  const allFilter = items.map((el) => el[key]);
+  const groupedObj:GroupsMap<T> = {};
+  const uniqueKeys
+  = allFilter.filter((value, index, self) => self.indexOf(value) === index);
+
+  uniqueKeys.forEach((item) => {
+    groupedObj[String(item)]
+    = items.filter((el) => el[key] === item);
+  });
+
+  return groupedObj;
 }
