806df43bb117dda1774322a0b6dc435a75303443
Max Skorokhod <mrzandras@gmail.com>
2022-11-14 14:17:41 +0200

    groupByKeys

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((total: GroupsMap<T>, next) => {
+    const trueTotal = total;
+    const trueNext = next[key] as string;
+
+    if (!trueTotal[trueNext]) {
+      trueTotal[trueNext] = [];
+    }
+
+    trueTotal[trueNext].push(next);
+
+    return trueTotal;
+  }, {});
 }
