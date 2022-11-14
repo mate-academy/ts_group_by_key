4e28d16caf5d9128b958bed82989b6a0c09a0343
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-14 10:26:41 +0200

    tests passed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const groupedObjectsByKey: GroupsMap<T> = {};
+
+  for (let i: number = 0; i < items.length; i += 1) {
+    const keyName = items[i][key] as string;
+
+    if (!groupedObjectsByKey[keyName]) {
+      groupedObjectsByKey[keyName] = [];
+    }
+
+    groupedObjectsByKey[keyName].push(items[i]);
+  }
+
+  return groupedObjectsByKey;
 }
