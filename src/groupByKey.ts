50e15e8dc2af9bda252258b531d21a66e51fd22d
Andrey Olin <crazy106ras@gmail.com>
2022-11-15 15:21:59 +0200

    fix after review

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   items.forEach((item) => {
