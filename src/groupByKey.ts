ef0d4a8eeded078dbe5899a8121c0f2ed6b84001
Spirit <baolt.king@gmail.com>
2022-11-19 18:25:01 +0200

    fix formatting

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,15 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T) :GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const allFilter = items.map((el) => el[key]);
-  const groupedObj:GroupsMap<T> = {};
-  const uniqueKeys
-  = allFilter.filter((value, index, self) => self.indexOf(value) === index);
+  const groupedObj: GroupsMap<T> = {};
+  const uniqueKeys = allFilter
+    .filter((value, index, self) => self.indexOf(value) === index);
 
   uniqueKeys.forEach((item) => {
-    groupedObj[String(item)]
-    = items.filter((el) => el[key] === item);
+    groupedObj[String(item)] = items
+      .filter((el) => el[key] === item);
   });
 
   return groupedObj;
