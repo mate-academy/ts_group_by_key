b9af6b4c1e8b94f06e151db7247f23f713b33ea1
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-19 23:48:07 +0200

    Done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,18 @@
+/* eslint-disable no-param-reassign */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((result: GroupsMap<T>, item) => {
+    const property = String(item[key]);
+
+    if (!Object.prototype.hasOwnProperty.call(result, property)) {
+      result[property] = [];
+    }
+
+    result[property].push(item);
+
+    return result;
+  }, {});
 }
