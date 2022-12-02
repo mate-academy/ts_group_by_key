94e276bc7675b1c56b2856b988638f56cf702382
July Terekhova <sky206380@gmail.com>
2022-12-02 21:49:26 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,17 @@
+/* eslint-disable array-callback-return */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const result: GroupsMap<T> = {};
+
+  items.map((el) => {
+    const newKey: string = `${el[key]}`;
+
+    result[newKey] = items.filter((item) => item[key] === el[key]);
+  });
+
+  return result;
 }
