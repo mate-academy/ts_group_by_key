a43c827596f6944e6f7937059747b01fb6d4825f
Kolya Movchan <kmovchan261@gmail.com>
2022-12-02 21:54:25 +0200

    Fixing any type

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,11 +5,12 @@ type GroupsMap<T> = {
 export function groupByKey<T, K extends keyof T>(items: T[], key: K)
   : GroupsMap<T> {
   return items.reduce((prev, current) => {
-    const thisKey = [current[key]];
+    const newKey: unknown = current[key];
 
     return {
       ...prev,
-      [thisKey as any]: items.filter((object) => object[key] === current[key]),
+      [newKey as string]: items.filter((object) => object[key]
+        === current[key]),
     };
   }, {});
 }
