fa09aa0bde1853fdc32c44ee7a470e09844d6274
Kolya Movchan <kmovchan261@gmail.com>
2022-12-02 20:52:57 +0200

    ts_group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,14 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T, K extends keyof T>(items: T[], key: K)
+  : GroupsMap<T> {
+  return items.reduce((prev, current) => {
+    const thisKey = [current[key]];
+
+    return {
+      ...prev,
+      [thisKey as any]: items.filter((object) => object[key] === current[key]),
+    };
+  }, {});
 }
