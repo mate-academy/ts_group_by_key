36d3e45c985048e4b9c3d70fad1add8b4cec1cf1
Oleksandr Filo <filo.sanya@gmail.com>
2022-12-08 20:27:10 +0200

    objects were grouped

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T):GroupsMap<T> {
+  const groupsMap: GroupsMap<T> = {};
+
+  items.forEach((element) => {
+    const newKey: string = `${element[key]}`;
+
+    if (!(newKey in groupsMap)) {
+      groupsMap[newKey] = [element];
+    } else {
+      groupsMap[newKey].push(element);
+    }
+  });
+
+  return groupsMap;
 }
