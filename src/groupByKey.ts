a9951c23ab5e67f21c2e2c60224e105ceef8f1b2
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-20 00:53:41 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,24 @@
-type GroupsMap<T> = {
+export interface GroupsMap<T> {
   [key: string]: T[];
-};
+}
 
-export function groupByKey(items, key) {
-  // write code here;
+export type KeyTypes = string | number;
+
+export interface Item {
+  [key: string]: KeyTypes;
+}
+
+export function groupByKey(items: Item[], key: keyof Item): GroupsMap<Item> {
+  const newKeys: KeyTypes[] = [];
+
+  items.forEach((item: Item) => {
+    if (!newKeys.includes(item[key])) {
+      newKeys.push(item[key]);
+    }
+  });
+
+  return newKeys.reduce((prev: GroupsMap<Item>, newKey: KeyTypes) => ({
+    ...prev,
+    [newKey]: items.filter((item: Item) => item[key] === newKey),
+  }), {});
 }
