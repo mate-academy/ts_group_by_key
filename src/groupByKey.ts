fc1380e7ccbed8870b6685cec0f0445405ffd332
Panianchuk M <panianchuk@gmail.com>
2022-12-21 01:07:22 +0200

    done1

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const group: GroupsMap<T> = {};
+
+  items.forEach((el) => {
+    const value = String(el[key]);
+
+    if (!group[value]) {
+      group[value] = [];
+    }
+
+    group[value].push(el);
+  });
+
+  return group;
 }
