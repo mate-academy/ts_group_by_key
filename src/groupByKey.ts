daab5bd6ed8e68902e5e479aee5231111b1c295e
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-21 08:45:52 +0200

    added groupByKey function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,27 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<I>(items: I[], key: keyof I): object {
+  let groupKeys = items.map((item: I) => item[key]);
+
+  groupKeys
+    = groupKeys
+      .filter((value, index: number) => groupKeys.indexOf(value) === index);
+
+  const maps
+    = groupKeys
+      .map((groupKey) => {
+        const map: GroupsMap<I>
+          = {
+            [`${groupKey}`]: items.filter((item : I) => item[key] === groupKey),
+          };
+
+        return map;
+      });
+
+  const result = {};
+
+  maps.forEach((map) => Object.assign(result, map));
+
+  return result;
 }
