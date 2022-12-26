9fc189e058265255b9d89cb82e550969df46ebe2
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-26 22:17:56 +0200

    changed my solution to the fastest method

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,26 +3,17 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<I>(items: I[], key: keyof I): object {
-  let groupKeys = items.map((item: I) => item[key]);
+  const groupedObj: GroupsMap<I> = {};
 
-  groupKeys
-    = groupKeys
-      .filter((value, index: number) => groupKeys.indexOf(value) === index);
+  items.forEach((item) => {
+    const value = String(item[key]);
 
-  const maps
-    = groupKeys
-      .map((groupKey) => {
-        const map: GroupsMap<I>
-          = {
-            [`${groupKey}`]: items.filter((item : I) => item[key] === groupKey),
-          };
+    if (!(value in groupedObj)) {
+      groupedObj[value] = [];
+    }
 
-        return map;
-      });
+    groupedObj[value].push(item);
+  });
 
-  const result = {};
-
-  maps.forEach((map) => Object.assign(result, map));
-
-  return result;
+  return groupedObj;
 }
