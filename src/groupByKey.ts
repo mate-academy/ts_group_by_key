9e9aa014beb161e6de94398172a2afd031781f2a
Maks <comanderius@gmail.com>
2022-12-28 12:27:36 +0100

    Group dy key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,20 @@
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  for (let i = 0; i < items.length; i += 1) {
+    const value = String(items[i][key]);
+
+    if (Object.prototype.hasOwnProperty.call(result, value)) {
+      result[value].push(items[i]);
+    } else {
+      Object.assign(result, { [value]: [items[i]] });
+    }
+  }
+
+  return result;
 }
