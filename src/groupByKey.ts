b98689b0a46c60dee6e8e7379dc7c72a572b9df5
Sergey Mironenko <serg1prime@gmail.com>
2023-01-15 03:39:01 +0200

    Group by key task: sixth commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,14 +3,13 @@ type GroupsMap<T> = {
 };
 
 type Group = {
-  [key: string]: any;
+  [key: string]: number | string;
 };
 
 export function groupByKey(items: Group[], key: string): GroupsMap<Group> {
   const result: GroupsMap<Group> = {};
-  const newItems = [...items].sort((current, prev) => current[key] - prev[key]);
 
-  newItems.map((el): void => {
+  items.map((el): void => {
     if (!(el[key] in result)) {
       result[`${el[key]}`] = [];
     }
