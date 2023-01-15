4923e4fa0a40c7b7909181447f16e40befa8eb56
Sergey Mironenko <serg1prime@gmail.com>
2023-01-15 02:51:05 +0200

    Task was fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -12,14 +12,14 @@ export function groupByKey(items: Obj[], key: string): GroupsMap<Obj> {
 
   newItems.sort((current, prev) => current[key] - prev[key]);
 
-  newItems.map((el): null => {
+  newItems.map((el): void => {
     if (!(el[key] in result)) {
       result[`${el[key]}`] = [];
     }
 
     result[`${el[key]}`].push(el);
 
-    return null;
+    return undefined;
   });
 
   return result;
