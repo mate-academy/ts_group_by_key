0d426f416f7e5471f7ace4225b431ab8f79a77c2
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-19 17:10:09 +0200

    task done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T):GroupsMap<T> {
+  const result:GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const groupKey = String(item[key]);
+
+    result[groupKey] = [];
+  });
+
+  items.forEach((item) => {
+    const groupKey = String(item[key]);
+
+    if (Object.hasOwnProperty.call(result, groupKey)) {
+      result[groupKey].push(item);
+    }
+  });
+
+  return result;
 }
