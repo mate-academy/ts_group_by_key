ad6ba84864735a56041a3b07a1d1c278c23e8d13
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-19 23:55:23 +0200

    solved task

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     '@typescript-eslint', 'jest',
   ],
   rules: {
+    'no-console': 'off',
     'arrow-body-style': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
