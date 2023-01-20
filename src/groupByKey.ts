69b09dfddc0b58c259ace8bc025f2c62a24a011f
Brandon Sunrise <stepansky93@gmail.com>
2023-01-20 21:35:49 +0200

    corrected

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,10 +4,14 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
-  const findKeys: Set<any> = new Set(items.map((item: T) => item[key]));
 
-  findKeys.forEach((keyFrom) => {
-    result[keyFrom] = items.filter((item:T) => item[key] === keyFrom);
+  items.forEach((item) => {
+    const itemKey = String(item[key]);
+
+    if (!(itemKey in result)) {
+      result[itemKey] = [];
+    }
+    result[itemKey].push(item);
   });
 
   return result;
