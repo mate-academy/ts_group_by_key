dc4328c7f23cb5a39165e3f0dbcf202c539bcf85
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-01-23 18:22:42 +0200

    fix remarks

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,8 +3,8 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  return items.reduce((prev, user: T) => ({
+  return items.reduce<GroupsMap<T>>((prev, user: T) => ({
     ...prev,
-    [user[key] as string]: items.filter((val: T) => val[key] === user[key]),
-  }), {} as GroupsMap<T>);
+    [`${user[key]}`]: items.filter((val: T) => val[key] === user[key]),
+  }), {});
 }
