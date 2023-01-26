230b7964a7f0ffba1bc1d7973294c4a97d5298f4
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-26 17:22:24 +0200

    change logic for one forEach

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,19 +3,13 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T):GroupsMap<T> {
-  const result:GroupsMap<T> = {};
+  const result: GroupsMap<T> = {};
 
   items.forEach((item) => {
     const groupKey = String(item[key]);
 
-    result[groupKey] = [];
-  });
-
-  items.forEach((item) => {
-    const groupKey = String(item[key]);
-
-    if (Object.hasOwnProperty.call(result, groupKey)) {
-      result[groupKey].push(item);
+    if (!result[groupKey]) {
+      result[groupKey] = items.filter((elem) => groupKey === String(elem[key]));
     }
   });
 
