7506db2c11db3f132516ccbddd1bc82b3a1d95c4
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2023-01-28 19:47:21 +0200

    fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -9,7 +9,7 @@ export function groupByKey<T, K extends keyof T>(items: T[], key:K)
   const result = items.reduce((prev, item) => {
     const valueOfKey = <GroupsMapKey>item[key];
     const itemsByValueKey = items.filter((filterItem) => {
-      return filterItem[key] === item[key];
+      return filterItem[key as keyof T] === item[key as keyof T];
     });
     const obj:GroupsMap<T> = {};
 
