83ced80a07cffd76514dfccc37a3cc7de9b78a30
Deniskakaka <krollik1000000@gmail.com>
2023-01-30 19:56:01 +0200

    fix

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,15 +4,16 @@ type GroupsMap<T> = {
 
 export function groupByKey
 <T>(items: T[], key: keyof T): GroupsMap<T> {
-  // i know it is not good, but what type it will be?
   const result: { [key: string]: T[keyof T] } = {};
 
   for (let i: number = 0; i < items.length; i += 1) {
-    if (typeof result[items[i][key]] === 'undefined') {
+    const groupKey = items[i][key] as string;
+
+    if (!result[items[i][key]]) {
       result[items[i][key]] = [];
     }
 
-    if (items[i][key] as unknown as 'string' in result) {
+    if (groupKey in result) {
       result[items[i][key]].push(items[i]);
     }
   }
