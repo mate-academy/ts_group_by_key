e8eed664124d04d905c100dcff37af8e07e21a39
Helena Ver <hellena84@i.ua>
2023-02-10 15:19:38 +0100

    commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((data) => {
+    const group = String(data[key]);
+
+    if (!result[group]) {
+      result[group] = [];
+    }
+
+    if (result[group]) {
+      result[group].push(data);
+    }
+  });
+
+  return result;
 }
