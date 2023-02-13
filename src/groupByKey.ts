70736de8870bdd8fc0714ae2141ff3d653764fcf
Maryna Kucher <marina.kucher8@gmail.com>
2023-02-13 12:40:32 +0200

    Solution was fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,18 +3,15 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  const values: string[] = items.map((obj) => String(obj[key]));
-  const uniqValues: string[] = [];
   const result: GroupsMap<T> = {};
 
-  values.forEach((item) => {
-    if (!uniqValues.includes(item)) {
-      uniqValues.push(item);
-    }
-  });
+  items.forEach((item) => {
+    const newKey = String(item[key]);
 
-  uniqValues.forEach((v) => {
-    result[v] = items.filter((obj) => String(obj[key]) === v);
+    if (!(newKey in result)) {
+      result[newKey] = [];
+    }
+    result[newKey].push(item);
   });
 
   return result;
