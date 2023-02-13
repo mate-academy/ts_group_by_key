5a4b843444c9ee9501d710f53ed6e9b700a52552
Vladyslav Krainyk <vladkraynyk2701@gmail.com>
2023-02-13 16:40:39 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyOfItem = String(item[key]);
+
+    if (!result[keyOfItem]) {
+      result[keyOfItem] = [];
+    }
+
+    result[keyOfItem].push(item);
+  });
+
+  return result;
 }
