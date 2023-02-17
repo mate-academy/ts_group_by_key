02ada6338023fb255d424232d6eb0dfa818007d1
Taras Baran <baranprivat22@gmail.com>
2023-02-17 22:55:47 +0200

    finished

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const currKey = item[key].toString();
+
+    if (result[currKey] === undefined) {
+      result[currKey] = [item];
+    } else {
+      result[currKey].push(item);
+    }
+  });
+
+  return result;
 }
