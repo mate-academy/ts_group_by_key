e4fd86401bf6fa7ec200f729f842cfd6bb7fb4bc
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-02-24 16:33:43 +0200

    Improved code style

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,14 +4,10 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groupsMap: GroupsMap<T> = {};
+  const values: Set<string> = new Set(items.map((item) => String(item[key])));
 
-  const uniqueKeys: Set<string> = new Set(
-    items.map((item) => String(item[key])),
-  );
-
-  Array.from(uniqueKeys).forEach((uniqueKey) => {
-    groupsMap[uniqueKey] = items
-      .filter((item) => String(item[key]) === uniqueKey);
+  Array.from(values).forEach((value) => {
+    groupsMap[value] = items.filter((item) => String(item[key]) === value);
   });
 
   return groupsMap;
