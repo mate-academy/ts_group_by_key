600fc2ac12092379d3ac2acfd1f849fa1423880c
Vasyl Pavlenko <genlus@ukr.net>
2023-02-24 12:57:07 +0200

    Task was solved

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const group: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyForElement = `${item[key]}`;
+
+    if (!(keyForElement in group)) {
+      group[keyForElement] = [];
+    }
+
+    group[keyForElement].push(item);
+  });
+
+  return group;
 }
