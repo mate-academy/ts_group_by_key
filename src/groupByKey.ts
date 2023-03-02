5bcd4dbbca1524e76302e58be473f3c212d1761c
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-03-02 20:02:10 +0200

    group by key solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,34 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const value = String(item[key]);
+
+    if (!result[value]) {
+      result[value] = [];
+    }
+
+    result[value].push(item);
+  });
+
+  return result;
+}
+
+//  it is just to try do it with reduce
+export function groupByKeyReduce<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((sortedObj: GroupsMap<T>, item): GroupsMap<T> => {
+    const value = String(item[key]);
+
+    if (!sortedObj[value]) {
+      // eslint-disable-next-line no-param-reassign
+      sortedObj[value] = [];
+    }
+
+    sortedObj[value].push(item);
+
+    return sortedObj;
+  }, {});
 }
