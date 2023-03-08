a1ad45ff7c092d65fa495029f41e32d63e292c8f
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-08 20:04:21 +0200

    Added types

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,8 +5,8 @@ type GroupsMap<T> = {
 export function groupByKey<T>(items: T[], key: keyof T & string): GroupsMap<T> {
   const groups: GroupsMap<T> = {};
 
-  items.forEach((item) => {
-    const groupKey = item[key];
+  items.forEach((item: T) => {
+    const groupKey: T [keyof T & string] = item[key];
 
     if (groups[groupKey as string]) {
       groups[groupKey as string].push(item);
