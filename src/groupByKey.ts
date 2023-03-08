1f4e786521aa6a03d33804471ccd07fdf8d99110
Rashyd <rashydlezgi@gmail.com>
2023-03-08 14:58:00 +0200

    function for grouping by key was created

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,19 @@
 type GroupsMap<T> = {
-  [key: string]: T[];
+  [key: string | number]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedByKey: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const itemKeyValue = item[key] as string;
+
+    if (!Object.prototype.hasOwnProperty.call(groupedByKey, itemKeyValue)) {
+      groupedByKey[itemKeyValue] = [];
+    }
+
+    groupedByKey[itemKeyValue].push(item);
+  });
+
+  return groupedByKey;
 }
