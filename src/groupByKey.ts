d9d0a902b19e6bac420efdb556bac5b367287d53
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-08 21:07:21 +0200

    Fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,12 +6,12 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groups: GroupsMap<T> = {};
 
   items.forEach((item: T) => {
-    const groupKey: T[keyof T] = item[key];
+    const groupKey: string = item[key];
 
     if (groupKey in groups) {
-      groups[groupKey as string].push(item);
+      groups[groupKey].push(item);
     } else {
-      groups[groupKey as string] = [item];
+      groups[groupKey] = [item];
     }
   });
 
