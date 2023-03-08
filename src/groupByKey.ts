0d904347d19f29a46bfaf9d08513a2970f26c136
Mark Babich <morak.bobama@gmail.com>
2023-03-08 18:49:36 +0200

    GroupByKey task done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+  const keysSet: Set<string> = new Set(items.map((item) => String(item[key])));
+  const keys: Array<string> = Array.from(keysSet);
+
+  keys.reduce((groupedItem, uniqKey): GroupsMap<T> => {
+    groupedItems[uniqKey] = items.filter((item) => {
+      return String(item[key]) === uniqKey;
+    });
+
+    return groupedItem;
+  }, groupedItems);
+
+  return groupedItems;
 }
