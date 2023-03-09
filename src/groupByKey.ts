4d22ae6dcfc58462c8613b7390916dbd21dca413
gretoq <dovhun3d@gmail.com>
2023-03-09 14:11:49 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(arr: T[], key: keyof T): GroupsMap<T> {
+  return arr.reduce((grouped: GroupsMap<T>, item: T) => {
+    const value = String(item[key]);
+    const copyGrouped = { ...grouped };
+
+    copyGrouped[value] ??= [];
+
+    copyGrouped[value].push(item);
+
+    return copyGrouped;
+  }, {});
 }
