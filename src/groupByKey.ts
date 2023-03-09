3cf54b2763caad4d13a7521276e1acd47f18f988
Kateryna Babina <katerynababina01@gmail.com>
2023-03-09 10:51:32 +0200

    fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,16 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T) : GroupsMap<T> {
-  return items.reduce((map:GroupsMap<T>, item: T) => {
-    const groupField = item[key] as string;
-    const groupObjectCopy: GroupsMap<T> = { ...map };
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((groupsMap: GroupsMap<T>, item: T) => {
+    const groupField = String(item[key]);
+    const groupObjectCopy: GroupsMap<T> = { ...groupsMap };
 
-    if (Object.prototype.hasOwnProperty.call(groupObjectCopy, groupField)) {
-      groupObjectCopy[groupField].push(item);
-    } else {
-      groupObjectCopy[groupField] = [item];
+    if (!{}.hasOwnProperty.call(groupObjectCopy, groupField)) {
+      groupObjectCopy[groupField] = [];
     }
+
+    groupObjectCopy[groupField].push(item);
 
     return groupObjectCopy;
   }, {});
