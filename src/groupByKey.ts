04f4524baf5f6d117b5ad95f23fb5768b49ed81a
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-10 13:11:14 +0100

    groups objects by keys

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((groupedObject: GroupsMap<T>, anotheObj) => {
+    const copyedGroupedObject = groupedObject;
+    const groupedKey = String(anotheObj[key]);
+
+    if (!copyedGroupedObject[groupedKey]) {
+      copyedGroupedObject[groupedKey] = [];
+    }
+
+    copyedGroupedObject[groupedKey].push(anotheObj);
+
+    return copyedGroupedObject;
+  }, {});
 }
