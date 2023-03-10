a914252ef6d7d457e9c44ed2e7980181a65834f0
andrej <andersontwentytwo@gmail.com>
2023-03-10 13:16:04 +0200

    Added check to avoid unneseccary array loop

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -8,6 +8,10 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   items.forEach((item) => {
     const groupBy = String(item[key]);
 
+    if (groupBy in groupedItems) {
+      return;
+    }
+
     groupedItems[groupBy] = items.filter(
       (element) => String(element[key]) === groupBy,
     );
