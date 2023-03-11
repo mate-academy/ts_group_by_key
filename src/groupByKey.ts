002fbbf0d3c1f0d00003d67af5b893a7304654ca
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2023-03-12 01:03:53 +0200

    solution for ts_group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,10 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  return items.reduce((acc, obj) => {
+    acc[obj[key]] = (acc[obj[key]] || []).concat(obj);
+
+    return acc;
+  }, {});
 }
