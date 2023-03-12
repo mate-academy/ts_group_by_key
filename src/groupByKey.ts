c2d019087c5caaf809dd71fb0b9601100198dea3
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2023-03-12 23:50:36 +0200

    implement changes requested

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((acc, obj) => {
     acc[obj[key]] = (acc[obj[key]] || []).concat(obj);
 
