e1c058f1c202e1dc736d282a24ac60898c9c4d82
Oleksiy Uchaev <corob.dir@gmail.com>
2023-03-18 09:51:49 +0200

    group_key_solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,11 @@
-type GroupsMap<T> = {
-  [key: string]: T[];
-};
+type GroupsMap<T> = {[key: string]: T[]};
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((acc: GroupsMap<T>, currObject) => {
+    const value = String(currObject[key]);
+
+    (acc[value] = acc[value] || []).push(currObject);
+
+    return acc;
+  }, {});
 }
