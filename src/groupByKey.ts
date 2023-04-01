4a7e79ac6eb9dd3ccfdf2e1a7ea61d018ab3fa9f
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-04-01 15:49:11 +0300

    now student and books can be sorted

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,31 @@
+// import { type } from "os";
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type Item = {
+  [key: string]: string | number;
+};
+
+export function groupByKey(items: Item[], key: string): GroupsMap<Item> {
+  const sortedObject: GroupsMap<Item> = items.reduce((
+    obj: GroupsMap<Item>,
+    item:Item,
+  ) => {
+    return {
+      ...obj,
+      [item[key]]: [],
+    };
+  }, {});
+
+  items.forEach((item: Item) => {
+    const property: string | number = item[key];
+
+    if (property in sortedObject) {
+      sortedObject[property].push(item);
+    }
+  });
+
+  return sortedObject;
 }
