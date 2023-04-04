494db26f7506b2e9121f4b6df3df4fe61618779c
Roman Kvas <fallenmadara@192.168.1.6>
2023-04-04 08:49:47 +0200

    Task Done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,21 @@
+/* eslint-disable no-console */
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const objItems: GroupsMap<T> = {};
+
+  items.forEach((elem) => {
+    const keyElemet = elem[key].toString();
+
+    if (!objItems[keyElemet]) {
+      objItems[keyElemet] = [];
+    }
+
+    objItems[keyElemet].push(elem);
+  });
+
+  return objItems;
 }
