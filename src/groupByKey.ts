f4fc7b07558caca68c49e1059d7c2bcc2b7b77c7
Anna Latusha <annapop03072701@gmail.com>
2023-04-06 20:26:46 +0300

    Improve

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,12 +2,9 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T extends Record<string, any>>(
-  items: T[],
-  key: keyof T,
-): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((groups, item) => {
-    const value = item[key];
+    const value = String(item[key]);
     const groupsCopy = { ...groups };
 
     if (!groupsCopy[value]) {
