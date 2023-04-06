95550820af01877e1671787d316ea9ea90a1459e
Ihor <ikruzhynskyy@gmail.com>
2023-04-06 12:07:20 +0300

    fixed as required

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,14 +3,14 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T):GroupsMap<T> {
-  return items.reduce((acc: GroupsMap<T>, el) => {
-    const keyValue = el[key] as string;
+  return items.reduce((acc: GroupsMap<T>, element) => {
+    const keyValue = String(element[key]);
 
     if (!acc[keyValue]) {
       acc[keyValue] = [];
     }
 
-    acc[keyValue].push(el);
+    acc[keyValue].push(element);
 
     return acc;
   }, {});
