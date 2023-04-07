968cc7bad0c59a3bc5a7defc1f54f26350ab5014
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-04-07 14:12:12 +0300

    grouped by keys

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+  const groupKeys: string[] = [];
+
+  Object.values(items).forEach((itemKey) => {
+    if (String(itemKey[key])) {
+      groupKeys.push(String(itemKey[key]));
+    }
+  });
+
+  groupKeys.forEach((groupKey: string) => {
+    result[groupKey] = items
+      .filter((item: T) => String(item[key]) === groupKey);
+  });
+
+  return result;
 }
