3d941151b314e6c100fe81dc6f95560b61bb86ce
Kacper Łyczba <suaby1725@gmail.com>
2023-05-04 12:15:02 +0200

    fixes

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,21 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<
-  T, R extends keyof T
->(
-  items: T[], key: R,
+export function groupByKey<T>(
+  items: T[], key: keyof T,
 ): GroupsMap<T> {
   // write code here;
-  const result: GroupsMap<T> = {};
+  const groupedByKey: GroupsMap<T> = {};
 
-  for (let i: number = 0; i < items.length; i += 1) {
-    const curr = items[i][key];
+  items.forEach((item) => {
+    const curr = item[key];
 
-    if (!(String(curr) in result)) {
-      result[String(curr)] = items.filter((item) => item[key] === curr);
+    if (!(String(curr) in groupedByKey)) {
+      groupedByKey[String(curr)] = items.filter((thing) => thing[key] === curr);
     }
-  }
+  });
 
-  return result;
+  return groupedByKey;
 }
