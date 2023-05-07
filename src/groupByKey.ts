947e679161d826c1916fd70e26fe068b3f38a658
Anna Grechana <grechanaanna@gmial.com>
2023-05-07 22:58:25 +0300

    1st version

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(
+  items:GroupsMap<string | number>,
+  key:string,
+):GroupsMap<string | number> {
+  const resalt:GroupsMap<string | number> = {};
+
+  items.forEach((item) => {
+    if (!resalt[item[key]]) {
+      resalt[item[key]] = [];
+    }
+    resalt[item[key]].push(item);
+  });
+
+  return resalt;
 }
