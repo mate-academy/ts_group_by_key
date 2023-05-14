aa9a038001a39fc998b5b86dbfca9d1510763999
Oleksandr Voloshyn <voloshyn244@gmail.com>
2023-05-14 23:48:07 +0200

    groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,19 @@
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const objItems: GroupsMap<T> = {};
+
+  items.forEach((elem) => {
+    const keyZnachenia = elem[key].toString();
+
+    if (!objItems[keyZnachenia]) {
+      objItems[keyZnachenia] = [];
+    }
+    objItems[keyZnachenia].push(elem);
+  });
+
+  return objItems;
 }
