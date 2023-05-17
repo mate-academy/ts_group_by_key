5ff483b4b4c34e807bf693cc66e1e9980046f0af
Victor Buldenko <gookidoo@gmail.com>
2023-05-17 18:50:39 +0300

    TS(2322)... make me cry)

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((el) => {
+    const propValue = `${el[key]}`;
+
+    if (result[propValue]) {
+      result[propValue].push(el);
+    } else {
+      result[propValue] = [];
+      result[propValue].push(el);
+    }
+  });
+
+  return result;
 }
