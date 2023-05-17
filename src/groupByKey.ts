a4abd959a6f3468f844381ffd6decaa763a10e71
Olena Shevchenko <shevochkas@gmail.com>
2023-05-17 14:49:15 +0300

    code function groupByKey was improved

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,14 +3,14 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
-  let arr: string[] = [];
   const result: GroupsMap<T> = {};
 
-  arr = items.map((item) => item[key]);
-  arr = [...new Set(arr)];
-
-  arr.forEach((item) => {
-    result[item] = items.filter((one) => one[key] === item);
+  items.forEach((item) => {
+    if (!(item[key] in result)) {
+      result[item[key]] = [item];
+    } else {
+      result[item[key]].push(item);
+    }
   });
 
   return result;
