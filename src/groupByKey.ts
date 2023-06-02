e761054c3a15f2bba3638682c510cdd1753ebb44
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-06-03 00:13:17 +0300

    ts_group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  // const group = {} as GroupsMap<T>;
+
+  const group = items.reduce((books: GroupsMap<T>, book) => {
+    const value = String(book[key]);
+
+    if (!books[value]) {
+      // eslint-disable-next-line no-param-reassign
+      books[value] = [];
+    }
+
+    books[value].push(book);
+
+    return books;
+  }, {});
+
+  return group;
 }
