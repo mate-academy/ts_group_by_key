f3eda84abf20236da9b57c4661b28d79c7a0cf60
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-06-12 14:14:49 +0300

    Solution: version 2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -9,10 +9,10 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
     const value = String(el[key]);
 
     if (value in groupedObject) {
-      return;
+      groupedObject[value].push(el);
+    } else {
+      groupedObject[value] = [el];
     }
-
-    groupedObject[value] = items.filter((item) => String(item[key]) === value);
   });
 
   return groupedObject;
