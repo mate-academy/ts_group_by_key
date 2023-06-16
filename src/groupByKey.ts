7d63980e7051caf6a805e735fdaa8681058a9c27
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-16 13:59:01 +0300

    code

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
+  const groupItem: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const newItem = item[key] as unknown as string;
+
+    if (newItem in groupItem) {
+      groupItem[newItem].push(item);
+    } else {
+      groupItem[newItem] = [item];
+    }
+  });
+
+  return groupItem;
 }
