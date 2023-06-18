3e26b93e94ada553c73e6a08d891aaf22aa45a83
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-06-18 18:29:21 +0300

    Task resolved

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: [], key: string): GroupsMap<object> {
+  let obj: GroupsMap<object> = {};
+
+  items.forEach((item) => {
+    obj = {
+      ...obj,
+      [item[key]]: items.filter((e) => e[key] === item[key]),
+    };
+  });
+
+  return obj;
 }
