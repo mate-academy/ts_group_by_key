d39617ddc91bafe880bb46890a2111d17d9a89fb
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-06-19 10:46:08 +0300

    fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,16 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
-  const result: GroupsMap<T> = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const grouped: GroupsMap<T> = {};
 
-  items.forEach((item: any) => {
-    if (!result[item[key]]) {
-      result[item[key]] = [item];
+  items.forEach((item) => {
+    const groupKey = String(item[key]);
+
+    if (!grouped[groupKey]) {
+      grouped[groupKey] = [item];
     } else {
-      result[item[key]].push(item);
+      grouped[groupKey].push(item);
     }
   });
 
-  return result;
+  return grouped;
 }
