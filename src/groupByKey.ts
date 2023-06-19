d363c0871462f147436bd120df13dcb429f87509
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-06-19 13:38:54 +0300

    Fixed after review

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,13 +3,12 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey(items: [], key: string): GroupsMap<object> {
-  let obj: GroupsMap<object> = {};
+  const obj: GroupsMap<object> = {};
 
   items.forEach((item) => {
-    obj = {
-      ...obj,
-      [item[key]]: items.filter((e) => e[key] === item[key]),
-    };
+    const groupKey = item[key];
+
+    obj[groupKey] = items.filter((e) => e[key] === groupKey);
   });
 
   return obj;
