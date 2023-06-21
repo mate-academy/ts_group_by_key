a155a027c53dd276d4f26b5765026041ccbf0269
Ilona Romaniuk <ilona09041983@gmail.com>
2023-06-21 17:05:01 +0300

    Group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedObj: GroupsMap<T> = {};
+
+  items.forEach((obj: T) => {
+    const property: string = `${obj[key]}`;
+
+    if (!groupedObj[property]) {
+      groupedObj[property] = [obj];
+    } else {
+      groupedObj[property].push(obj);
+    }
+  });
+
+  return groupedObj;
 }
