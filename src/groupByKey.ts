1aba7a836973e794e9ba6110d4619bd43ef0c9aa
Evhen Ivchenko <ivchenkoevhen@gmail.com>
2023-06-23 19:42:31 +0300

    finished

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((objGroup: GroupsMap<T>, obj: T) => {
+    const objKey: string = String(obj[key]);
+    const result = objGroup;
+
+    if (objKey in result) {
+      result[objKey].push(obj);
+    } else {
+      result[objKey] = [obj];
+    }
+
+    return result;
+  }, {});
 }
