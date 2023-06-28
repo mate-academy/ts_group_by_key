0aa208856d3c40839c113a192954d0ac66842bda
IlonaBorkovska <borkovska.io@gmail.com>
2023-06-28 11:43:52 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,40 @@
-type GroupsMap<T> = {
-  [key: string]: T[];
+type GroupsMap<S, B> = {
+  [key: string]: Array<S | B>;
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+interface Book {
+  author: string;
+  country: string;
+  imageLink: string;
+  language: string;
+  pages: number;
+  title: string;
+  year: number;
+}
+
+export function groupByKey(
+  items: Array<Student | Book>,
+  key: string,
+): GroupsMap<Student, Book> {
+  const groupItems: GroupsMap<Student, Book> = {};
+
+  items.forEach((item: (Student | Book)): void => {
+    const groupKey:string = item[key as keyof (Student | Book)];
+
+    if (!groupItems[groupKey]) {
+      groupItems[groupKey] = [];
+    }
+
+    groupItems[groupKey]?.push(item);
+  });
+
+  return groupItems;
 }
