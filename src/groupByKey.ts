d22f91923f4414293defe7079c14e721ad191707
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-30 13:59:50 +0300

    sol

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], initialKey: string) : GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+  const keys: string[] = items.map((item) => item[initialKey]);
+
+  keys.forEach((key) => {
+    groupedItems[key] = items.filter((item) => item[initialKey] === key);
+  });
+
+  return groupedItems;
 }
