01693129542146a3aee13683a95621e3183b82a5
Nataliia Koreniuk <koreniuknataliia@gmail.com>
2023-07-01 19:11:27 +0300

    changed array name

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,9 +6,9 @@ export function groupByKey(items: [], key: string): GroupsMap<{}> {
   const result: GroupsMap<{}> = {};
 
   for (let i = 0; i < items.length; i += 1) {
-    const sort = items.filter((obj) => obj[key] === items[i][key]);
+    const arraySortedByKey = items.filter((obj) => obj[key] === items[i][key]);
 
-    result[items[i][key]] = sort;
+    result[items[i][key]] = arraySortedByKey;
   }
 
   return result;
