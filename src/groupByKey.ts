0c4cd53f613be75248fc3efe2c2df1c6c1668219
Diana Tuz <dianatuz12@gmail.com>
2023-07-08 11:22:35 +0300

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: [], key: string): GroupsMap<Object> {
+  const group: GroupsMap<Object> = {};
+
+  items.forEach((item) => {
+    if (item[key]) {
+      if (!group[item[key]]) {
+        group[item[key]] = [];
+      }
+    }
+    group[item[key]].push(item);
+  });
+
+  return group;
 }
