7b506c86a5c79df361b777e0f04a230b1f34c1c0
Shevchenko Oleksandr <54949928+Vayts@users.noreply.github.com>
2023-07-17 18:23:23 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,7 +6,7 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const resultObject: GroupsMap<T> = {};
 
   items.forEach((item) => {
-    const objKey = item[key] as unknown as string;
+    const objKey = item[key] as string;
 
     if (!resultObject[objKey]) {
       resultObject[objKey] = [item];
