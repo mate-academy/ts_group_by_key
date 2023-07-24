ce879450e71a001238e90a883d8b42fa6005fbbc
Dmytro Fefelov <reseneweb@gmail.com>
2023-07-24 16:31:06 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function
+groupByKey<T, K extends keyof T>(items: T[], key: K): GroupsMap<T> {
+  const result: GroupsMap<T> = items.reduce((acc, ch) => {
+    const keyValue = ch[key] as unknown as string;
+
+    if (acc[keyValue] === undefined) {
+      acc[keyValue] = [];
+    }
+    acc[keyValue].push(ch);
+
+    return acc;
+  }, {} as GroupsMap<T>);
+
+  return result;
 }
