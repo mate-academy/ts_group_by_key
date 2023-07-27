63ae2a31d58ac98c9425cb35b7a9b3f6745441fa
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-27 10:06:17 +0200

    Implement task groupByKey.ts

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,29 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<ObjectToSort, Key extends keyof ObjectToSort>(
+  items: ObjectToSort[],
+  key: Key,
+): GroupsMap<ObjectToSort> {
+  if (items.length === 0) {
+    return {};
+  }
+
+  type ValueFromObject = ObjectToSort[Key];
+
+  const valuesOfKey: Array<ValueFromObject> = items.map(
+    (item: ObjectToSort) => item[key],
+  );
+  const uniqueValuesOfKey: Set<ValueFromObject> = new Set([...valuesOfKey]);
+  const sortedObjects: GroupsMap<ObjectToSort> = {};
+
+  uniqueValuesOfKey.forEach((uniqueValueOfKey: ValueFromObject) => {
+    const relativesObjects = items.filter(
+      (item: ObjectToSort) => item[key] === uniqueValueOfKey,
+    );
+
+    sortedObjects[String(uniqueValueOfKey)] = relativesObjects;
+  });
+
+  return sortedObjects;
 }
