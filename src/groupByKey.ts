0556c0a7c392930d0b96d19b21173943031f9b16
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-08-16 16:16:50 +0200

    add sort method

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,21 @@
 type GroupsMap<T> = {
-  [key: string]: T[];
+  [key: string]: T[]; // Array<T>
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const currentKey: string = String(item[key]);
+
+    if (!result[currentKey]) {
+      result[currentKey] = [];
+    }
+    result[currentKey].push(item);
+  });
+
+  return result;
 }
