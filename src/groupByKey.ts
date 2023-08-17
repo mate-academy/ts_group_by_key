0362e6f1eb4aae68fef1458138bf21f886a17747
Andrey Bondar <andrey_bondar@outlook.com>
2023-08-17 10:21:29 +0300

    task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,23 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: Array<T>, key: keyof T): GroupsMap<T> {
+  const uniqValues: string[] = [];
+  const result: GroupsMap<T> = {};
+
+  /* eslint-disable-next-line */
+  for (const obj of items) {
+    const keyValue = obj[key] as unknown as string;
+
+    if (!uniqValues.includes(keyValue)) {
+      uniqValues.push(keyValue);
+    }
+  }
+
+  /* eslint-disable-next-line */
+  for (const key1 of uniqValues) {
+    result[key1] = items.filter((obj) => obj[key] === key1);
+  }
+
+  return result;
 }
