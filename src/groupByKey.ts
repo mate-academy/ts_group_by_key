1af8a471e312799f30bfd07e62c4db3d2cf0c846
arturwisnia <wisnia3008@interia.pl>
2023-08-20 21:15:42 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,22 @@
-type GroupsMap<T> = {
-  [key: string]: T[];
-};
+export function groupByKey<T>(items: T[], key: keyof T): object {
+  const groupsMap = items.reduce((map, item) => {
+    const keyValue = item[key];
+    const newGroup = map.get(keyValue);
 
-export function groupByKey(items, key) {
-  // write code here;
+    if (newGroup) {
+      newGroup.push(item);
+    } else {
+      map.set(keyValue, [item]);
+    }
+
+    return map;
+  }, new Map<any, T[]>());
+
+  const groups: Record<string, T[]> = {};
+
+  groupsMap.forEach((group, b) => {
+    groups[b] = group;
+  });
+
+  return groups;
 }
