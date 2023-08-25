830cb504935feb62c7241479a50d617abe94504f
Orchibald <orestkozak17@gmail.com>
2023-08-25 11:20:27 +0200

    group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const grouped: GroupsMap<T> = {};
+
+  /* eslint-disable-next-line */
+  for (const item of items) {
+    const keyValue = item[key] as string;
+
+    if (!grouped[keyValue]) {
+      grouped[keyValue] = [];
+    }
+
+    grouped[keyValue].push(item);
+  }
+
+  return grouped;
 }
