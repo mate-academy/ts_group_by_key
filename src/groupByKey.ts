504cbeb2fb5eb68f43792b461682f6a0ea276265
Anton Tsurkan <wesses1999@gmail.com>
2023-08-29 22:57:12 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const sortedItemsGroup: GroupsMap<T> = {};
+
+  items.reduce((acc: GroupsMap<T>, el: T) => {
+    if (!(el[key] in acc)) {
+      acc[el[key]] = [];
+    }
+
+    acc[el[key]].push(el);
+
+    return acc;
+  }, sortedItemsGroup);
+
+  return sortedItemsGroup;
 }
