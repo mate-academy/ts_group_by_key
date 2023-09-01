b10a2c79c9dc4f811597c1bfe26d37068dd99640
daviinnchi <zaikoivan99@gmail.com>
2023-09-01 14:21:09 +0200

    Implemented groupping by key logic

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], searchedKey: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+  const searchedKeys = new Set(items.map((value) => value[searchedKey]));
+
+  searchedKeys.forEach((key) => {
+    result[key] = items.filter((item) => item[searchedKey] === key);
+  });
+
+  return result;
 }
