519bc420eb2293e4fe7b8c5d49b54295903f871d
Taras Voievoda <voevoda.taras@gmail.com>
2023-09-03 14:17:03 +0300

    added functionality for groupByKey function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const groupedKey = item[key];
+
+    if (!groupedItems[groupedKey as string]) {
+      groupedItems[groupedKey as string] = [];
+    }
+
+    groupedItems[groupedKey as string].push(item);
+  });
+
+  return groupedItems;
 }
