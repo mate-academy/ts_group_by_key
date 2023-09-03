0f058eff6746d2e6efb200befd75a9944defdab3
Yuliia Antonenko <shinjisan311@gmail.com>
2023-09-03 19:18:11 +0300

    fixed code style

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T)
+  : GroupsMap<T> {
+  const sortedItems: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const keyToSort = `${item[key]}`;
+
+    if (!sortedItems[keyToSort]) {
+      sortedItems[keyToSort] = [];
+    }
+
+    sortedItems[keyToSort].push(item);
+  });
+
+  return sortedItems;
 }
