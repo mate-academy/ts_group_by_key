4c3a9b5695432534a84e0f3471d1ba89dc854c5f
AndrMar1939 <advo737@gmail.com>
2023-09-04 17:14:03 +0300

    change condition in .reduce and formated types

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,18 +6,14 @@ interface Item {
   [key: string]: any;
 }
 
-export function groupByKey(items :Item[], key: string)
-  :GroupsMap<Item> {
-  const groupedByKey = items.reduce((acc :GroupsMap<Item>, current :Item) => {
-    const newKey :string = current[key];
+export function groupByKey(items: Item[], key :keyof GroupsMap<Item>):
+GroupsMap<Item> {
+  const groupedByKey = items.reduce((acc: GroupsMap<Item>, current: Item) => {
+    const newKey: string = current[key];
 
-    if (acc[newKey]) {
-      acc[newKey].push(current);
-
-      return acc;
+    if (!acc[newKey]) {
+      acc[newKey] = [];
     }
-
-    acc[newKey] = [];
 
     acc[newKey].push(current);
 
