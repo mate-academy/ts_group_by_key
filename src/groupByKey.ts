137c9dea7e250f1cdf41a937753178b3cb34f6c9
Yevheniia Sidanchenko <sidanchenkoe@gmail.com>
2023-09-05 20:25:27 +0300

    implementing groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items :T[], key: keyof T) :GroupsMap<T> {
+  return items.reduce((acc:GroupsMap<T>, item: T) => {
+    const keyForGroup = item[key].toString();
+
+    if (!acc[keyForGroup]) {
+      acc[keyForGroup] = [];
+    }
+
+    acc[keyForGroup].push(item);
+
+    return acc;
+  }, {});
 }
