29ed1d915192cd91069da4bb9894d0656ec10638
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-09-05 18:28:30 +0300

    add group by key function logic

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((acc, item) => {
+    const itemKey = String(item[key]);
+
+    if (!acc[itemKey]) {
+      acc[itemKey] = [];
+    }
+    acc[itemKey].push(item);
+
+    return acc;
+  }, {} as GroupsMap<T>);
 }
