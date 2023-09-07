9f244dd731a0ada8fa7e33238fc58fa4277e0469
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-09-07 12:06:13 +0300

    fixed variables names and add types to arguments

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -3,14 +3,14 @@ type GroupsMap<T> = {
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  return items.reduce((acc, item) => {
-    const itemKey = String(item[key]);
+  return items.reduce((acc: GroupsMap<T>, item: T) => {
+    const itemGroup = String(item[key]);
 
-    if (!acc[itemKey]) {
-      acc[itemKey] = [];
+    if (!acc[itemGroup]) {
+      acc[itemGroup] = [];
     }
-    acc[itemKey].push(item);
+    acc[itemGroup].push(item);
 
     return acc;
-  }, {} as GroupsMap<T>);
+  }, {});
 }
