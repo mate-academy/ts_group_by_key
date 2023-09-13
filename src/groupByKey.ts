65b959935c1716ccfec166f9038100ac61e84fb7
Anton Ustavshchykov <unton84@gmail.com>
2023-09-13 15:56:04 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,10 @@
 type GroupsMap<T> = {
-  [key: string]: T[];
+  [key: string]: T[],
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((sum: GroupsMap<T>, obj) => ({
+    ...sum,
+    [obj[key] as string]: (sum[obj[key] as string] || []).concat(obj),
+  }), {});
 }
