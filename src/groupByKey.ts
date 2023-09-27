88e7aef3a68beff48376be9b0baa55c823bb02ca
stunnerboyone <stunnerboy691@gmail.com>
2023-09-27 18:44:58 +0300

    groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  const sortedItems: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const itemKey = String(item[key]);
+
+    if (!sortedItems[itemKey]) {
+      sortedItems[itemKey] = [];
+    }
+
+    sortedItems[itemKey].push(item);
+  });
+
+  return sortedItems;
 }
