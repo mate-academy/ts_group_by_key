ee21f38554ee03d7117196a3dfb4766f250cc973
Mykhailo <eglet777@ukr.net>
2023-10-11 11:02:08 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,23 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const arrOfUniqueValues: T[keyof T][] = [];
+  const groupsMap: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    if (!arrOfUniqueValues.includes(item[key])) {
+      arrOfUniqueValues.push(item[key]);
+    }
+  });
+
+  arrOfUniqueValues.forEach((value: T[keyof T]) => {
+    groupsMap[String(value)] = [];
+  });
+
+  items.forEach((item: T) => {
+    groupsMap[String(item[key])].push(item);
+  });
+
+  return groupsMap;
 }
