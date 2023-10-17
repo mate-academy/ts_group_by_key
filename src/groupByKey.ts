0f3804e87bc5fd6d23d6a844359fed3146cfacd5
Yuliia Bakun <bakunyuliia@gmail.com>
2023-10-17 11:58:57 +0200

    ts-groupByKey task implementation

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: Array<T>, key: keyof T): GroupsMap<T> {
+  const grouppedData: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyToGroupBy = String(item[key]);
+
+    if (keyToGroupBy in grouppedData) {
+      grouppedData[keyToGroupBy].push(item);
+    } else {
+      grouppedData[keyToGroupBy] = [item];
+    }
+  });
+
+  return grouppedData;
 }
