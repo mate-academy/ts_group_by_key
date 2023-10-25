ddd9853c642ad4be1e1ef83c610c11f41a864f58
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-10-25 19:27:13 +0200

    Changed code after review

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,7 +6,7 @@ export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
   const newObj: GroupsMap<T> = {};
 
   items.forEach((el, index, arr) => {
-    const sort: T[] = arr.filter((otherBook) => otherBook[key] === el[key]);
+    const sort: T[] = arr.filter((otherEl) => otherEl[key] === el[key]);
 
     newObj[el[key]] = sort;
   });
