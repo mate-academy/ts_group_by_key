7a7bc22a322cd731d21b6e810f072c4119159adf
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-10-25 18:59:35 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const newObj: GroupsMap<T> = {};
+
+  items.forEach((el, index, arr) => {
+    const sort: T[] = arr.filter((otherBook => otherBook[key] === el[key]));
+    newObj[el[key]] = sort;
+})
+
+  return newObj;
 }
