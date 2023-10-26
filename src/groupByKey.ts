7940ef1d5040ad49a46c8ef8316a823e0ba61afc
Danylo Artomov <daniilart01@gmail.com>
2023-10-26 23:35:41 +0300

    implementation

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends Object>(items: T[],
+  key: keyof T): GroupsMap<T> {
+  const groups: GroupsMap<T> = {};
+
+  const groupNames: Set<string>
+    = new Set([...(items.map((obj: T) => String(obj[key])))]);
+
+  groupNames.forEach((group: string) => {
+    groups[group] = items.filter((item: T) => String(item[key]) === group);
+  });
+
+  return groups;
 }
