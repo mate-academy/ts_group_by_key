e592d8ddacada7f96add50d53db118611145fd30
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-11-02 17:37:11 +0200

    group-by-key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,24 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<
+  T, K extends keyof T,
+>(items: T[], key: K): GroupsMap<T> {
+  const arrPartItems: string[] = [];
+  const objSortItems: GroupsMap<T> = {};
+
+  items.forEach((element) => {
+    if (element[key]) {
+      const keyValue = element[key] as any;
+
+      if (!arrPartItems.includes(keyValue)) {
+        arrPartItems.push(keyValue);
+        objSortItems[keyValue] = [];
+      }
+
+      objSortItems[keyValue].push(element);
+    }
+  });
+
+  return objSortItems;
 }
