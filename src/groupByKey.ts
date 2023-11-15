4fb859dcfe11c8fd05f921217d96cab7012d0cc3
Denys Vasylenkoe <kapodenim@gmail.com>
2023-11-15 20:50:00 +0200

    v1

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T):GroupsMap<T> {
   // write code here;
+  const result: GroupsMap<T> = {};
+  const distinct = [...items.map((item:T) => `${item[key]}`)];
+
+  for (let i = 0; i < distinct.length; i += 1) {
+    result[distinct[i]] = items.filter((item:T) => `${item[key]}`
+    === distinct[i]);
+  }
+
+  return result;
 }
