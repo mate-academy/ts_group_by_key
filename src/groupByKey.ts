05fcd19309014f715be9017328b3ecb2b5fcf3b3
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-11-19 18:50:53 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,30 @@
+/* eslint-disable no-restricted-syntax */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const arrOfValues: string[] | number[] = [];
+  const result: GroupsMap<T> = {};
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of items) {
+    if (!arrOfValues.includes(item[key])) {
+      arrOfValues.push(item[key]);
+    }
+  }
+
+  for (const ch of arrOfValues) {
+    result[ch] = [];
+  }
+
+  for (const ch of arrOfValues) {
+    for (const item of items) {
+      if (item[key] === ch) {
+        result[ch].push(item);
+      }
+    }
+  }
+
+  return result;
 }
