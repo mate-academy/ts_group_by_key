25a3e863ac958a4e20b693a08ab8a670cd42ac0f
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-11-20 09:51:24 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,19 +5,15 @@ type GroupsMap<T> = {
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const item of items) {
+  items.forEach((item) => {
     const value: string | number = item[key];
 
-    if (result[value] !== undefined) {
-      result[value].push(item);
+    if (!result[value]) {
+      result[value] = [];
     }
 
-    if (result[value] === undefined) {
-      result[value] = [];
-      result[value].push(item);
-    }
-  }
+    result[value].push(item);
+  });
 
   return result;
 }
