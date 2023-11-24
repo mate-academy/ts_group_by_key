30fc1d65d8004eef21799411ac613d4bb90c84a7
Yurii Hlushenko <yauglushenko@gmail.com>
2023-11-24 10:47:24 +0200

    second commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,14 +2,12 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T[]): GroupsMap<T> {
-  const keys: string| number[] = [];
+export function groupByKey<T>(items: [], key: keyof T[]): GroupsMap<T> {
+  const keys: string | number[] = [];
 
-  items.map((item) => (keys.includes(item[key])
-    ? null
-    : keys.push(item[key])));
+  items.forEach((item) => (!keys.includes(item[key]) && keys.push(item[key])));
 
-  const result = {};
+  const result: GroupsMap<T> = {};
 
   keys.map((k) => (result[k] = items.filter((el) => el[key] === k)));
 
