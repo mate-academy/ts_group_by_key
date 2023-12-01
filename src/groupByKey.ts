1952fe24dfef4da8b7e7be6d7c956c7659ade84a
Oleksandr Kosenko <d1izel@ukr.net>
2023-12-01 16:08:44 +0200

    add ts_group_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const groupItems: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const keyValue = item[key] as string;
+
+    groupItems[keyValue] = groupItems[keyValue] || [];
+
+    groupItems[keyValue].push(item);
+  });
+
+  return groupItems;
 }
