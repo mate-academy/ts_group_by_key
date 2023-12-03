afd3551c6fc2c5fe63e31b2fe996e7bf66d4c026
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-03 19:24:03 +0200

    Revert "Grouped by keys"
    This reverts commit 5abf2f074633d9196d94916f5eee34f890a0dced.

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,16 +2,6 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
-  const group: GroupsMap<T> = {};
-
-  const keyValues = items.map((item) => item[key]);
-
-  keyValues.forEach((value) => {
-    if (!(String(value) in group)) {
-      group[String(value)] = items.filter((item) => item[key] === value);
-    }
-  });
-
-  return group;
+export function groupByKey(items, key) {
+  // write code here;
 }
