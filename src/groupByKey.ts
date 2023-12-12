a8416be8ad68ac00eb7d8c14ee4e24ecd9a03050
Ruslan Hrynchuk <r.momos@gmail.com>
2023-12-12 14:17:43 +0200

    Tests passed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item: T): void => {
+    const valueStr = String(item[key]);
+
+    if (!result[valueStr]) {
+      result[valueStr] = [];
+    }
+
+    result[valueStr].push(item);
+  });
+
+  return result;
 }
