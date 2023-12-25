86ed65b9accfce7fd8e85f49f7edf1cd6124e45e
boytrend0108 <boytrend@gmail.com>
2023-12-25 15:01:44 +0200

    complete task

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const res: GroupsMap<T> = {};
+
+  items.forEach((el) => {
+    const sortBy = el[key] as keyof GroupsMap<T>;
+
+    if (!res[sortBy]) {
+      res[sortBy] = [el];
+    } else {
+      res[sortBy] = [...res[sortBy], el];
+    }
+  });
+
+  return res;
 }
