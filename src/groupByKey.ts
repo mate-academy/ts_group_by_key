6e3f2833473583279cc4a3f9dcf70245a72ccb81
Inna Sheremet <129211319+InnaSh23@users.noreply.github.com>
2023-12-27 18:16:54 +0200

    Update src/groupByKey.ts
    Co-authored-by: Nazar Matsevych <80700247+nazarmatsevych@users.noreply.github.com>

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,7 +4,7 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
   return items.reduce<GroupsMap<T>>((acc, item) => {
-    const keyValue = item[key] as unknown as string;
+    const keyValue = item[key].toString();
 
     if (!acc[keyValue]) {
       acc[keyValue] = [];
