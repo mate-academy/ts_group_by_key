492edb178396a8c8eef6ef0890ec8d39366c3ab0
Kriss Kozak <misskrissretro@gmail.com>
2023-12-29 00:13:50 +0200

    ts_group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((groups, item) => {
+    const keyValue = item[key] as string;
+
+    return {
+      ...groups,
+      [keyValue]: [...(groups[keyValue] || []), item],
+    };
+  }, {} as GroupsMap<T>);
 }
