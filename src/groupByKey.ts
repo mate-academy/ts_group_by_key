b5f06f74b622226d46d034a24ec4cd25494f3e4d
Kyryb <kyrybcode@gmail.com>
2024-01-08 23:55:00 +0200

    gropByKey function implemented

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T extends object>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
   // write code here;
+  const groupedItems: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const itemValue = item[key] as string;
+
+    if (!groupedItems[itemValue]) {
+      groupedItems[itemValue] = [];
+    }
+    groupedItems[itemValue].push(item);
+  });
+
+  return groupedItems;
 }
