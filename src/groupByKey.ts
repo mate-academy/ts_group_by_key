31e0e21cdcb50f403085bd4e6c437b82e458270c
RVDotsenko <docenkoromaha@gmail.com>
2024-01-10 04:41:23 +0200

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,26 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groups: GroupsMap<T> = {};
+
+  const sortedField = items.map((a: T) => a[key]);
+
+  const groupsSortedField: typeof sortedField = [];
+
+  for (let i: number = 0; i < sortedField.length; i += 1) {
+    if (!groupsSortedField.includes(sortedField[i])) {
+      groupsSortedField.push(sortedField[i]);
+    }
+  }
+
+  for (let i: number = 0; i < groupsSortedField.length; i += 1) {
+    // eslint-disable-next-line
+    const field = '' + groupsSortedField[i];
+
+    groups[field] = items
+      .filter((a: T) => a[key] === groupsSortedField[i]);
+  }
+
+  return groups;
 }
