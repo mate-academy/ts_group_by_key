d490c4be75b71c0bc1550e114a08066cbe8953c6
Artem <artemmalikov742@gmail.com>
2024-01-12 09:26:47 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedObjects: GroupsMap<T> = {};
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of items) {
+    const groupField = String(item[key]);
+
+    if (!groupedObjects[groupField]) {
+      groupedObjects[groupField] = [];
+    }
+
+    groupedObjects[groupField].push(item);
+  }
+
+  return groupedObjects;
 }
