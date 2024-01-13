cf8222fc9daf65005a6beb62da47781ff08f59c7
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-01-13 18:41:49 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
+  const sortedObjects: { [key: string]: R[] } = {};
+
+  items.forEach((obj: R) => {
+    if ((obj[key] as string) in sortedObjects) {
+      sortedObjects[obj[key] as string].push(obj);
+    } else {
+      sortedObjects[obj[key] as string] = [obj];
+    }
+  });
+
+  return sortedObjects;
 }
