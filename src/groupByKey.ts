1442d59d67804315475cee5414f015db0a7cfe91
Anton Duchenko <deragandi0@gmail.com>
2024-01-13 18:47:10 +0200

    add one variable

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -7,10 +7,12 @@ export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
   const groupedItems: GroupsMap<R> = {};
 
   items.forEach((item: R) => {
-    if (item[key] as string in groupedItems) {
-      groupedItems[item[key] as string].push(item);
+    const itemOnIteration = item[key] as string;
+
+    if (itemOnIteration in groupedItems) {
+      groupedItems[itemOnIteration].push(item);
     } else {
-      groupedItems[item[key] as string] = [item];
+      groupedItems[itemOnIteration] = [item];
     }
   });
 
