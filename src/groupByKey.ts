fc142216e9fe90aafabf81010ff060f599b781b5
White EdenI <danil.gayvoronsky2201@gmail.com>
2024-01-15 23:25:22 +0200

    Done!(try1)

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<R>(items: R[], key: keyof R): GroupsMap<R> {
+  const groupedItems: GroupsMap<R> = {};
+
+  items.forEach((item: R) => {
+    const itemOnIteration = item[key] as string;
+
+    if (itemOnIteration in groupedItems) {
+      groupedItems[itemOnIteration].push(item);
+    } else {
+      groupedItems[itemOnIteration] = [item];
+    }
+  });
+
+  return groupedItems;
 }
