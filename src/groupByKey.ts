6291b079178f721e35c2cfca5bdb054ef065f271
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-01-22 17:00:44 +0100

    sol

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,40 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: [], key: string): GroupsMap<typeof items> {
+  const result: GroupsMap<typeof items> = {};
+
+  items.map((item) => {
+    result[item[key]] = [];
+
+    return result;
+  });
+
+  items.map((item) => {
+    Object.keys(item).forEach((k) => {
+      if (k === key) {
+        result[item[key]].push(item);
+      }
+    });
+
+    return result;
+  });
+
+  return result;
 }
+
+// for (const index: string of Object.keys(items)) {
+//   const item = items[index];
+
+//   if (item.hasOwnProperty([key])) {
+//     const value = item[key];
+
+//     if (!result[value]) {
+//       result[value] = [];
+//     }
+
+//     result[value].push(item);
+//   }
+
+//   return result;
+// }
