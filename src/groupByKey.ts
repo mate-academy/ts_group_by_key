b26a00f83032d8be976d4476b12d6808f27ca9aa
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-01-26 11:08:46 +0200

    solution task

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,30 @@
+/* eslint-disable no-param-reassign */
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce<GroupsMap<T>>((object, item: T) => {
+    const groupKey = item[key] as string;
+
+    object[groupKey] = items
+      .filter((itemFilter) => itemFilter[key] === item[key]);
+
+    return object;
+  }, {});
 }
+
+// const groupObject: GroupsMap<T> = {};
+
+//   items.forEach((obj: T) => {
+//     const groupKey = obj[key] as string;
+
+//     if (!groupObject[groupKey]) {
+//       groupObject[groupKey] = [];
+//     }
+
+//     groupObject[groupKey].push(obj);
+//   });
+
+//   return groupObject;
