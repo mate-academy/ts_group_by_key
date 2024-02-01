b65cacd9624506dcd642baf5f072bfba14d72fdd
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2024-02-01 15:30:10 +0200

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<Item>(
+  items: Item[],
+  key: keyof Item,
+): GroupsMap<Item> {
+  const newObj: GroupsMap<Item> = {};
+
+  for (let i = 0; i < items.length; i += 1) {
+    const currentItem = items[i];
+    const keyValue = currentItem[key] as string;
+
+    if (!newObj[keyValue]) {
+      newObj[keyValue] = items.filter((item) => item[key] === keyValue);
+    }
+  }
+
+  return newObj;
 }
