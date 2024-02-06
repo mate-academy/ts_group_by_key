9bfb530e6bb7c3ca648bd409e03281add14908cb
Valeriia Fomenko <valeriia.fomenko@ukr.net>
2024-02-06 19:46:22 +0200

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,7 +6,7 @@ export function groupByKey<T extends Record<string, any>>(
   items: T[],
   key: keyof T,
 ): GroupsMap<T> {
-  return items.reduce((groups: GroupsMap<T>, item: T) => {
+  return items.reduce<GroupsMap<T>>((groups, item: T) => {
     const keyValue = item[key] as string;
 
     return {
