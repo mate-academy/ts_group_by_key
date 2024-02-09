046d39b7697226f7ae752bb0097b7cac0067740c
Robert Szałapak <robsza1212@gmail.com>
2024-02-09 16:59:02 +0100

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items : T[], key : keyof T) : GroupsMap<T> {
+  const solution : GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const valueOfKey : string = String(item[key]);
+
+    if (!solution[valueOfKey]) {
+      solution[valueOfKey] = [];
+    }
+
+    solution[valueOfKey].push(item);
+  });
+
+  return solution;
 }
