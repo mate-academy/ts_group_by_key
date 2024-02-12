922e9dbb8362fca69fbd83977cb7d613a205465c
mrsvolodya <vovamursky@gmail.com>
2024-02-12 18:44:30 +0200

    develop

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: Array<T>, key: string): GroupsMap<T> {
+  const data = items.reduce((acc, currentObject) => {
+    const keyValue = currentObject[key];
+    const dataSave = { ...acc };
+
+    if (!dataSave[keyValue]) {
+      dataSave[keyValue] = [];
+    }
+    dataSave[keyValue].push(currentObject);
+
+    return dataSave;
+  }, {});
+
+  return data;
 }
