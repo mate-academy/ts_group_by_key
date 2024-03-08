230831fae5bb0505b0eabe630955f41bacb52942
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-03-08 16:44:26 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type T = {
+  [key: string]: any;
+};
+
+export function groupByKey(items: T[], key: string): GroupsMap<T> {
+  const groupedElements: GroupsMap<T> = {};
+
+  const keyValues: string[] = [...items].map((el) => el[key]);
+
+  keyValues.forEach((keyValue) => {
+    const result: T[] = [...items].filter((el) => el[key] === keyValue);
+
+    groupedElements[keyValue] = result;
+  });
+
+  return groupedElements;
 }
