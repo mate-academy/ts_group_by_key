e621225bb4085df9d369afc29105b4bcd42f1b6d
Justin <thedarkvibe1@gmail.com.com>
2024-03-12 16:57:07 +0100

    TaskComplete

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const itemsByKey: GroupsMap<T> = {};
+
+  items.forEach((item: T) => {
+    const itemValue = item[key as keyof T];
+
+    if (itemsByKey[itemValue as keyof GroupsMap<T>] === undefined) {
+      itemsByKey[itemValue as keyof GroupsMap<T>] = [];
+    }
+
+    itemsByKey[itemValue as keyof GroupsMap<T>].push(item);
+  });
+
+  return itemsByKey;
 }
