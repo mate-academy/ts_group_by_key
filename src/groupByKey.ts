d1460daf751858a6ba4dc79ba0406dded7cb3c56
Mari Andrushko <kap.mebel.a@gmail.com>
2024-03-13 17:50:37 +0100

    task

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,25 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends { [key: string]: unknown }>(
+  items: T[],
+  key: string,
+): GroupsMap<T> {
+  const res: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const keyValue = item[key];
+
+    if (!(typeof keyValue === 'string' || typeof keyValue === 'number')) {
+      throw new Error(`value for key: ${key} is not a string or number`);
+    }
+
+    if (!res.hasOwnProperty(keyValue)) {
+      res[keyValue] = [item];
+    } else {
+      res[keyValue].push(item);
+    }
+  }
+
+  return res;
 }
