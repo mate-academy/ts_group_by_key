6ced38d7a848b3ad4f8dc2878218d62a85739e8d
Ярина Пугач <puhachyaryna@gmail.com>
2024-03-14 12:20:19 +0100

    created TS function groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((acc, item) => {
+    const keyValue = item[key] as unknown as string;
+
+    if (!acc[keyValue]) {
+      acc[keyValue] = [item];
+    } else {
+      acc[keyValue].push(item);
+    }
+
+    return acc;
+  }, {} as GroupsMap<T>);
 }
