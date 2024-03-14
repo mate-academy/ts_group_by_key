50fd7d68f82c3b8e5d56ceffa4061925ab72d15c
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-03-15 00:28:45 +0200

    task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,23 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends Record<string, any>>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  return items.reduce((result: GroupsMap<T>, item: T) => {
+    const keyValue = item[key];
+
+    if (!result[keyValue]) {
+      return {
+        ...result,
+        [keyValue]: [item],
+      };
+    }
+
+    return {
+      ...result,
+      [keyValue]: [...result[keyValue], item],
+    };
+  }, {});
 }
