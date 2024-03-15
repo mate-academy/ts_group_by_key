443052c220fd0d707eaf8a84114124c0bcfaf2fc
Ярина Пугач <puhachyaryna@gmail.com>
2024-03-15 09:47:23 +0100

    created interface to specify types

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,9 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+interface KeyValuePairs {
+  [key: string]: string | number;
+}
+
+export function groupByKey<T extends KeyValuePairs>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
   return items.reduce((acc, item) => {
-    const keyValue = item[key] as unknown as string;
+    const keyValue = item[key];
 
     if (!acc[keyValue]) {
       acc[keyValue] = [item];
