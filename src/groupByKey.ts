cbde2b28cae41d9d43ba2df49359dbf831b17bce
Denys209818 <deniskravcuk63@gmail.com>
2024-03-15 20:36:35 +0200

    Load full realization

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: Array<T>, key: keyof T): GroupsMap<T> {
   // write code here;
+  const obj: GroupsMap<T> = {};
+
+  for (let i = 0; i < items.length; i++) {
+    const keyItem: string = items[i][key] as string;
+
+    if (obj[keyItem]) {
+      obj[keyItem].push(items[i]);
+    } else {
+      obj[keyItem] = [items[i]];
+    }
+  }
+
+  return obj;
 }
