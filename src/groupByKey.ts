b4987e868b354e1505cbab793ab70596dc6958c3
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-03-20 01:06:59 +0200

    Remove T type

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,15 +1,13 @@
-type T = { [key: string]: string };
-
-type GroupsMap = {
+type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items: T[], key: keyof T): GroupsMap {
-  return items.reduce((grouped: GroupsMap, item: T) => {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((grouped: GroupsMap<T>, item: T) => {
     const result = grouped;
-    const keyValue = item[key];
+    const keyValue = item[key] as string;
 
-    if (!grouped[keyValue]) {
+    if (!result[keyValue]) {
       result[keyValue] = [];
     }
     result[keyValue].push(item);
