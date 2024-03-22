78d2b4abe479d742f8e3a58389a3d5e0fdf3907c
Sergay Trytiak <trytik12@gmail.com>
2024-03-22 21:55:22 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const obj: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const itemsCopy: T[] = [...items];
+
+    const arg: any = item[key];
+
+    if (!obj[arg]) {
+      obj[arg] = itemsCopy.filter((a: T) => {
+        return a[key] === arg;
+      });
+    }
+  }
+
+  return obj;
 }
