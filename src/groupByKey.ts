4a5a427593dd92785afac51e474b2be910340235
Oleh Marushchak <westernpc22@gmail.com>
2024-03-22 23:26:41 +0200

    add test

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<Type>(
+  items: Type[],
+  key: keyof Type,
+): GroupsMap<Type> {
+  const result: GroupsMap<Type> = {};
+
+  items.map((item) => {
+    for (const prop of Object.keys(item)) {
+      if (prop === key) {
+        result[item[prop]] = items.filter((item1) => item[prop] === item1[key]);
+      }
+    }
+  });
+
+  return result;
 }
