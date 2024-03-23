f232cd46c81be1a87955602a67cdb7ea48bcc2ea
ihor_romaniuk <iromaniuk@mycubes.nl>
2024-03-23 22:16:07 +0200

    add solution and update test node version

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
   // write code here;
+  const res: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const resKey = item[key as keyof T] as string;
+
+    if (!Object.keys(res).includes(resKey)) {
+      Object.assign(res, {
+        [resKey]: items.filter((el) => el[key as keyof T] === resKey),
+      });
+    }
+  }
+
+  return res;
 }
