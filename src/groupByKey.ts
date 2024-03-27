e4f93ab3b4a3939ac775ed89d8af7336a14ae38d
MaximAV <atanasovmaksim56@gmail.com>
2024-03-27 20:23:32 +0200

    version 1.1

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,20 @@
+/* eslint-disable max-len */
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((value) => {
+    const keyValue = value[key];
+
+    if (result[keyValue]) {
+      result[keyValue].push(value);
+    } else {
+      result[keyValue] = [value];
+    }
+  });
+
+  return result;
 }
