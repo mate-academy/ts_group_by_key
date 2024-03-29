7bb1dc04d837363f8a9943fdf86006060507a5c9
Bohdan Shyrii <shyrii11bohdan@gmail.com>
2024-03-29 15:20:32 +0100

    farmed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,8 +5,12 @@ type GroupsMap<T> = {
 export function groupByKey<T, K extends keyof T>(
   items: T[],
   key: K,
-): GroupsMap<T> {
-  return items.reduce((acc: GroupsMap<T>, item: T) => {
+): GroupsMap<T>
+{
+  return items
+    .reduce((
+      acc: GroupsMap<T>,
+      item: T) => {
     const sortKey: string = String(item[key]);
 
     if (!acc[sortKey]) {
