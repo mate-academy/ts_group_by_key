59726a14736036f1116e7969f552165770aa8070
Zubkovskiy Bohdan <zubkovvsbogdan@gmail.com>
2024-04-03 13:54:19 +0300

    release

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const resultKey = String(item[key]);
+
+    if (!(resultKey in result)) {
+      result[resultKey] = [item];
+    } else {
+      result[resultKey] = [...result[resultKey], item];
+    }
+  });
+
+  return result;
 }
