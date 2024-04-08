d0a83272c666558d028fedac1a1a24e5b441d526
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-04-08 19:58:26 -0300

    first commit

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey(
+  items: [{ [key: string | number]: string }],
+  key: keyof GroupsMap<{ [key: string | number]: string }>,
+): object {
   // write code here;
+  return items.reduce(
+    (acc: GroupsMap<{ [key: string | number]: string }>, el) => {
+      if (acc[el[key]]) {
+        return acc;
+      }
+
+      return {
+        ...acc,
+        [el[key]]: items.filter((e) => e[key] === el[key]),
+      };
+    },
+    {},
+  );
 }
