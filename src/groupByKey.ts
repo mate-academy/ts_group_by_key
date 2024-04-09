dd6297a87680d1142d7522f7fcd058d624b99f13
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-04-09 09:33:39 -0300

    Reduce complexity

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -5,13 +5,14 @@ type GroupsMap<T> = {
 export function groupByKey<K>(items: K[], key: keyof K): GroupsMap<K> {
   // write code here;
   return items.reduce((acc: GroupsMap<K>, el) => {
-    if (acc[String(el[key])]) {
-      return acc;
+    if (!acc[String(el[key])]) {
+      acc[String(el[key])] = [];
     }
+
+    acc[String(el[key])].push(el);
 
     return {
       ...acc,
-      [String([el[key]])]: items.filter((e) => e[key] === el[key]),
     };
   }, {});
 }
