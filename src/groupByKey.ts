03b4fcf45fd79771434ad7a37ed0340c42f78a30
svyatoslav <svyatoslavik9@gmail.com>
2024-04-10 17:48:02 +0200

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,25 +2,11 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-type Book = {
-  id: number;
-  color: string;
-  country: string;
-};
-
-type Student = {
-  name: string;
-  surname: string;
-  age: number;
-  married: boolean;
-  grades: number[];
-};
-
-export function groupByKey(
-  items: Array<Book | Student>,
-  key: string,
+export function groupByKey<Item>(
+  items: Array<Item>,
+  key: keyof Item,
 ): GroupsMap<Object> {
-  // write code here;
+  // write code here ;
   const result: GroupsMap<typeof items> = {};
 
   items.forEach((item: any) => {
