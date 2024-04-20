1920f6b985c28d3ee714624b2100fb322be66fbe
kinqbert <perehodko1010@gmail.com>
2024-04-20 19:13:10 +0300

    removed hasOwn method usage

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -8,7 +8,7 @@ export function groupByKey(items: object[], key): GroupsMap<typeof items> {
   for (const item of items) {
     type ObjectType = typeof item;
 
-    if (!Object.hasOwn(result, item[key as keyof ObjectType])) {
+    if (!result.hasOwnProperty(item[key as keyof ObjectType])) {
       result[item[key as keyof ObjectType]] = [item as keyof ObjectType];
     } else {
       result[item[key as keyof ObjectType]].push(item as keyof ObjectType);
