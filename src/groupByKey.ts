9b6f3eade7d6b4968cb3584283ea9337d8a81a4c
metrakon <metrakon@proton.me>
2024-04-21 17:45:28 +0300

    add_solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items, key): GroupsMap<typeof items> {
+  const groupedItems: GroupsMap<typeof items> = {};
+
+  for (const item of items) {
+    const value = item[key];
+
+    if (!groupedItems[value]) {
+      groupedItems[value] = [];
+    }
+
+    groupedItems[value].push(item);
+  }
+
+  return groupedItems;
 }
