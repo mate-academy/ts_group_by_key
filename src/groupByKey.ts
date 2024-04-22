214dde8a8a5a6edd23d7ed77a5d4a2413fc5cfb7
metrakon <metrakon@proton.me>
2024-04-22 14:50:07 +0300

    fix

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,11 +2,14 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key): GroupsMap<typeof items> {
-  const groupedItems: GroupsMap<typeof items> = {};
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
+  const groupedItems: GroupsMap<T> = {};
 
   for (const item of items) {
-    const value = item[key];
+    const value = String(item[key]);
 
     if (!groupedItems[value]) {
       groupedItems[value] = [];
