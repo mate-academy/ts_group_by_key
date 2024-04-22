a5a851976559909d6b622eb70f4591b3f91cd813
kinqbert <perehodko1010@gmail.com>
2024-04-22 12:54:08 +0300

    task fixes

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,19 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items: object[], key: any): GroupsMap<typeof items> {
-  const result: GroupsMap<typeof items> = {};
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
 
   for (const item of items) {
-    type ItemType = typeof item;
+    const indexedKey = key as keyof T;
+    const indexedItem = item[indexedKey];
 
-    const indexedKey = key as keyof ItemType;
-    const indexedItem = item as keyof ItemType;
-
-    if (!result.hasOwnProperty(item[indexedKey])) {
-      result[item[indexedKey]] = [indexedItem];
+    if (!result.hasOwnProperty(String(indexedItem))) {
+      result[String(indexedItem)] = [item];
     } else {
-      result[item[indexedKey]].push(indexedItem);
+      result[String(indexedItem)].push(item);
     }
   }
 
