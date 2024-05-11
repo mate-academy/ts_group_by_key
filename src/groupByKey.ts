e80561319fcb28246a44527c257f80c7fb27cbcf
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-05-10 21:20:15 -0500

    GroupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: any = {};
+
+  for (const i of items) {
+    if (typeof result[`${i[key]}`] !== 'undefined') {
+      result[i[key]].push(i);
+    } else {
+      result[i[key]] = [];
+      result[i[key]].push(i);
+    }
+  }
+
+  return result;
 }
