e0c3b77aae533bc3fc9c4f6e61fff6105cc70db4
Iryna Zahorodnia <irishas25@ukr.net>
2024-05-13 15:19:28 +0300

    group by key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+type Item = {
+  [key: string]: string;
+};
+
+export function groupByKey(items: Item[], key: string): GroupsMap {
+  const groups: GroupsMap = {};
+
+  items.forEach((item) => {
+    const value = item[key];
+
+    if (!groups[value]) {
+      groups[value] = [];
+    }
+
+    groups[value].push(item);
+  });
+
+  return groups;
 }
