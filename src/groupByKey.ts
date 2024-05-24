5ab49f7642177bc5de2035c204eab54c8589a4d8
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-05-24 09:11:38 +0300

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: [], key: string): GroupsMap<string> {
+  const itemsValuesOfKeys: string[] = [];
+  const results: GroupsMap<string> = {};
+
+  for (const item of items) {
+    if (itemsValuesOfKeys.includes(item[key])) {
+      continue;
+    }
+    itemsValuesOfKeys.push(item[key]);
+  }
+
+  for (const el of itemsValuesOfKeys) {
+    const filtersItems = items.filter((item) => item[key] === el);
+
+    results[el] = filtersItems;
+  }
+
+  return results;
 }
