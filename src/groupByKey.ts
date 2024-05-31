873e2aaad51e67da1ca044969296e94b86d53319
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-05-31 15:23:55 +0300

    add

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const newObj: GroupsMap<T> = {};
 
   for (const item of items) {
