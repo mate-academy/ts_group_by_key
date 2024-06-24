b75c8674a9dc7511a1736caa2bc8c048423275a0
Hamid Faiza <hamid.faiza811@gmail.com>
2024-06-24 23:14:07 +0300

    fixed code after AI review

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,21 +1,18 @@
 type GroupsMap<T> = {
-  [key: string]: T[];
+  [key: string | number]: T[];
 };
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const groupedData: GroupsMap<T> = {};
-  const allKeysValues: Array<string> = [];
 
   for (const item of items) {
     const value = `${item[key]}`;
 
-    if (!allKeysValues.includes(value)) {
-      allKeysValues.push(value);
+    if (!groupedData.hasOwnProperty(value)) {
+      groupedData[value] = [item];
+    } else {
+      groupedData[value].push(item);
     }
-  }
-
-  for (const value of allKeysValues) {
-    groupedData[value] = items.filter((el) => `${el[key]}` === value);
   }
 
   return groupedData;
