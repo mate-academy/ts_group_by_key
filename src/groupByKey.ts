f551b49679479753a049520ab82b27934dabfcb4
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-30 18:25:19 +0300

    [Feature] added group by key function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends { [key: string]: string }>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  return items.reduce((acc, item) => {
+    const prev = acc[item[key]] ?? [];
+
+    return {
+      ...acc,
+      [item[key]]: [...prev, item],
+    };
+  }, {} as GroupsMap<T>);
 }
