39760935f77b378758b166df673aeadb9eca7e7a
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-06 11:25:05 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((result: GroupsMap<T>, item: T) => {
+    const groypKey = String(item[key]); // отримую значення ключа
+
+    if (!result[groypKey]) {
+      result[groypKey] = []; // ініціалізую масив якщо групи нема ще
+    }
+    result[groypKey].push(item); // додаю елемент до групи якої треба
+
+    return result;
+  }, {});
 }
