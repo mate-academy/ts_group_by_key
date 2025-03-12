5eacf46d6a6cbfcbdd8ff5c5aeaa8487b8fb63f8
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-12 20:52:45 +0200

    fix task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -7,15 +7,12 @@ export function groupByKey<T, K extends keyof T>(
   key: K,
 ): GroupsMap<T> {
   const grouped: GroupsMap<T> = {};
-  const keys = new Set(items.map((item) => item[key] as string));
 
   for (const item of items) {
     const itemKey = item[key] as string;
 
-    if (keys.has(itemKey)) {
-      grouped[itemKey] = grouped[itemKey] || [];
-      grouped[itemKey].push(item);
-    }
+    grouped[itemKey] = grouped[itemKey] || [];
+    grouped[itemKey].push(item);
   }
 
   return grouped;
