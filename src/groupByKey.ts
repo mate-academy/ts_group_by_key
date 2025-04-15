e702d3d1256e2b03d9a99ef5efd28e5f1cf49f17
larysa1387 <kaliuzhna.off@gmail.com>
2025-04-15 17:55:55 +0300

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends Record<string, any>>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  const itemsChecked = items.filter((item) => item[key] !== undefined);
+
+  return itemsChecked.reduce((acc, item) => {
+    const groupKey = item[key];
+
+    if (!acc[groupKey]) {
+      acc[groupKey] = [];
+    }
+    acc[groupKey].push(item);
+
+    return acc;
+  }, {} as GroupsMap<T>);
 }
