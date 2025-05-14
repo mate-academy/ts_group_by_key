02c4e45868298cf4e89a3291ce4afdf7db2b9e6e
Galyna Maikovych <gmaik.com@gmail.com>
2025-05-14 21:37:36 +0300

    done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: string): GroupsMap<T> {
+  const res: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const currentKey = String(item[key as keyof T]);
+
+    if (res[currentKey]) {
+      res[currentKey].push(item);
+    } else {
+      res[currentKey] = [item];
+    }
+  }
+
+  return res;
 }
