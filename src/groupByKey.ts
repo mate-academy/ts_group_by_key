195a4738ea06ee7a503fcd6876b350cdedc1a258
wanderlei jose bolzan <wbolzanjp@gmail.com>
2025-06-18 16:01:43 -0300

    first Version pull request about task TS_GROUP_BY_KEY

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const groupedResult: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const keyValue: string = String(item[key]);
+
+    if (groupedResult[keyValue]) {
+      groupedResult[keyValue].push(item);
+    } else {
+      groupedResult[keyValue] = [item];
+    }
+  }
+
+  return groupedResult;
 }
