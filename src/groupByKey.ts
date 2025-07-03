14202ceeafce052852c4f5c20ed80e5cd0be0c48
michelam-pseudo <michelammilian@outlook.com>
2025-07-03 18:53:15 -0300

    Implantação da função especificada na descrição do repositório, groupByKey

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,29 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+  if (items.length === 0) {
+    return {};
+  }
+
+  const objectGrouped: GroupsMap<T> = {};
+
+  const valuesOfKey: any[] = [];
+
+  items.forEach((item: T) => {
+    if (!valuesOfKey.includes(item[key])) {
+      valuesOfKey.push(item[key]);
+    }
+  });
+
+  valuesOfKey.forEach((valueOfKey: any): void => {
+    const valueOfKeyString = valueOfKey.toString();
+
+    objectGrouped[valueOfKeyString] = items.filter((item: T) => {
+      return item[key] === valueOfKey;
+    });
+  });
+
+  return objectGrouped;
 }
