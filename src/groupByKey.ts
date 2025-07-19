546ed6b0d039bbf021c79f6478ebcce1986bbf65
Taras <luchakt2@gmail.com>
2025-07-19 12:46:32 +0300

    changed key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,7 +4,7 @@ type GroupsMap<T, K extends keyof T> = {
 
 export function groupByKey<T, K extends keyof T>(
   items: T[],
-  key: keyof K
+  key: K
 ): GroupsMap<T, K> {
   const newArr = {} as GroupsMap<T, K>;
 
