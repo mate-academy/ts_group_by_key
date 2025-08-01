583a3d89d2992f1f49f6dd7b3fdabda70f998d32
Mykyta Musaiev <n.musaiev@gmail.com>
2025-08-01 14:43:30 +0300

    task done

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends Record<string, any>>(
+  arr: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  return arr.reduce((acc, curr) => {
+    const groupKey = String(curr[key]);
+
+    return {
+      ...acc,
+      [groupKey]: [...(acc[groupKey] || []), curr],
+    };
+  }, {} as GroupsMap<T>);
 }
