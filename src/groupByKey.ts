cdc9a2d1508bde2d1705e437c2e30809af5c3bf0
gOFFa <n4v4rm0re666@gmail.com>
2025-09-10 18:17:22 +0300

    fix so the T is item type

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,18 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T extends GroupsMap<T>>(
-  items: T[],
-  key: keyof T,
-): GroupsMap<T> {
-  return items.reduce((acc: GroupsMap<T>, item: T) => {
-    const groupKey: string = String(item[key]);
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce<GroupsMap<T>>((acc, item) => {
+    const groupKey = String(item[key]);
 
     if (!acc[groupKey]) {
       acc[groupKey] = [];
     }
+
     acc[groupKey].push(item);
 
     return acc;
-  }, {});
+  }, {} as GroupsMap<T>);
 }
