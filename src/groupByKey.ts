b009629f8f79bb1ba7acd2494a405b0aae0b6be7
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-09-19 21:33:09 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,10 +2,14 @@ export function groupByKey<T extends object>(
   items: T[],
   key: keyof T,
 ): Record<string, T[]> {
+  if (items.length === 0) return {};
+
   const result: Record<string, T[]> = {};
 
   for (const item of items) {
-    const keyValue = String(item[key]);
+    const keyValue = item[key] !== null && item[key] !== undefined
+      ? String(item[key])
+      : 'null_or_undefined';
 
     if (!result[keyValue]) {
       result[keyValue] = [];
