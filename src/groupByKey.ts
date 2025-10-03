95c1764ca8856050c39ae365035e9fa6f26fe1c2
S-Khraban <s.hraban@i.ua>
2025-10-03 15:06:28 +0300

    _create group map TS for groupByKey_

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,21 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
+  const groups: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const groupKey = String(item[key]);
+
+    if (!groups[groupKey]) {
+      groups[groupKey] = [];
+    }
+
+    groups[groupKey].push(item);
+  }
+
+  return groups;
 }
