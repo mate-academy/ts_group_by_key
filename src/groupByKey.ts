d873008afae4b0201d43525d922088538db77973
milton <mitnunes.pro.com>
2025-10-28 17:45:34 -0300

    add1

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends object, K extends keyof T>(
+  items: T[],
+   key: K
+  ): GroupsMap<T> {
+  return items.reduce((accumulator: GroupsMap<T>, item: T) => {
+     const groupValue = item[key];
+     const groupKey: string = String(groupValue);
+     
+     if (!accumulator[groupKey]) {
+       accumulator[groupKey] = []; 
+     }
+      accumulator[groupKey].push(item);
+     return accumulator;
+  }, {} as GroupsMap<T>);
 }
