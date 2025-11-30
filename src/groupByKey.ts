b3af4e2f95e5f0da96aa69b3f21531412579e2c1
Igor Tinyayev <tinyayev.ca@gmail.com>
2025-11-30 15:58:34 -0700

    ts_group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends Record<string, any>>(
+  items: T[],
+  key: keyof T,
+): GroupsMap<T> {
+  return items.reduce<GroupsMap<T>>((result, item) => {
+    const groupKey = String(item[key]);
+
+    if (!result[groupKey]) {
+      result[groupKey] = [];
+    }
+
+    result[groupKey].push(item);
+
+    return result;
+  }, {});
 }
