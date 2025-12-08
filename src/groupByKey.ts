167612745547ed585016b83bc8ad572806d1f370
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-12-08 12:01:39 +0200

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,32 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+// export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+//   return items.reduce((acc, item) => {
+//     const keyValue = String(item[key]); // приводимо до строки для ключа
+
+//     if (!acc[keyValue]) {
+//       acc[keyValue] = [];
+//     }
+
+//     acc[keyValue].push(item);
+
+//     return acc;
+//   }, {} as GroupsMap<T>);
+// }
+
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  items.forEach((item) => {
+    const keyValue = String(item[key]);
+
+    if (!result[keyValue]) {
+      result[keyValue] = [];
+    }
+
+    result[keyValue].push(item);
+  });
+
+  return result;
 }
