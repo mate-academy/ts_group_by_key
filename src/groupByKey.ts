3a0894043020d6f672ce477769ab43c682cd2d89
Lidiia Shvets <lepoki051003@gmail.com>
2026-02-07 18:44:21 +0100

    group_by_key

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const groupKey = item[key];
+    const groupKeyString = String(groupKey);
+
+    if (!result[groupKeyString]) {
+      result[groupKeyString] = [];
+    }
+
+    result[groupKeyString].push(item);
+  }
+
+  return result;
 }
