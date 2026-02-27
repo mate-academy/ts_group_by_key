8888920292f63a60e0f73c966611bd066a09701f
unknown <dkuz6323@gmail.com>
2026-02-27 11:52:13 +0200

    completed tasks

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { rmSync } from 'fs';
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+
+  const result: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const value = item[key];
+
+    const valueS = String(value);
+
+    if (!result[valueS]) {
+      result[valueS] = [];
+      result[valueS].push(item);
+    }
+  }
+
+  return result;
 }
