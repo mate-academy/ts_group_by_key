1743e585bee5d7b62c4293dd7a58289ec6917db1
Taras Yelahin <inflades@gmail.com>
2026-03-23 21:24:53 +0200

    ts_group_by_key_task

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,22 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce(
+    (groups, item) => {
+      const raw = item[key];
+
+      if (raw === null || raw === undefined) {
+        return groups;
+      }
+
+      const k = String(raw);
+
+      groups[k] = groups[k] ?? [];
+      groups[k].push(item);
+
+      return groups;
+    },
+    {} as Record<string, T[]>,
+  );
 }
