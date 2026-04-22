09eddbfe26d7ae4749a16f04ae4bd940269e831a
Artem <muha13artem@gmail.com>
2026-04-22 20:55:24 +0300

    first try

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -1,7 +1,24 @@
+'use strict';
+
 type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
+  const result: GroupsMap<T> = {};
+
+  for (const item of items) {
+    const groupName = String(item[key]);
+
+    if (!result[groupName]) {
+      result[groupName] = [];
+    }
+
+    result[groupName].push(item);
+  }
+
+  return result;
 }
