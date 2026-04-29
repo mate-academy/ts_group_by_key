20e41745f2bccd300fba5ab8840bf998099462a6
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-04-29 08:14:03 +0300

    tests passed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
+
+  return items.reduce((obj: GroupsMap<T>, item: T) => {
+    const objKey = String(item[key]);
+
+    if (!obj[objKey]) {
+      obj[objKey] = [];
+    }
+
+    obj[objKey].push(item);
+
+    return obj;
+  }, {});
 }
