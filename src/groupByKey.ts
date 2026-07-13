9a17a2ecd012938f017196fd310a65ad2b16f84e
Vitalii Hoshuliak <goshulyakvitalik@gmail.com>
2026-07-13 13:45:01 +0100

    13/44

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,7 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T): T {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = {};
 
   for (const item of items) {
